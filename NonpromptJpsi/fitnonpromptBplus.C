81575091bb388befff1f6e04024cd88a3c4a44a7
gian michele <ginnocen@cern.ch>
2014-04-20 18:22:43 -0400

    upd

--- NonpromptJpsi/fitnonpromptBplus.C
+++ NonpromptJpsi/fitnonpromptBplus.C
@@ -11,7 +11,7 @@ TString inputdata="../../output/myoutputBplus.root";
 TString inputmc="../../output/myoutputBplus.root";
 
 //TString cut="(HLT_PAMu3_v1)&&gen==1";
-TString cut="(HLT_PAMu3_v1)&&gen==1";
+TString cut="(HLT_PAMu3_v1)&&genB0==1";
 TString seldata=Form("abs(y+0.465)<1.93&&%s",cut.Data());
 
 TString selmc="1";
