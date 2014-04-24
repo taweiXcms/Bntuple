2e28fb2d2814a12d5ccf673ccb88a02e18e621d5
gian michele <ginnocen@cern.ch>
2014-04-24 11:45:01 -0400

    update

--- NonpromptJpsi/loopNonpromptBzero.C
+++ NonpromptJpsi/loopNonpromptBzero.C
@@ -197,38 +197,28 @@ void fillTree(TVector3* bP, TVector3* bVtx, TLorentzVector* b4P, int j, int type
 
   //gen info judgement
 
-  if(!REAL)
-    {
-      gen[typesize] = 0;//gen init
-      genIndex[typesize] = -1;//gen init
-      genpt[typesize] = -1;
-      geneta[typesize] = -20;
-      genphi[typesize] = -20;
-      geny[typesize] = -1;
-      int mGenIdxTk1=-1;
-      int mGenIdxTk2=-1;
-      int bGenIdxTk1=-1;
-      int bGenIdxTk2=-1;
-      int bGenIdxMu1=-1;
-      int bGenIdxMu2=-1;
+  if(!REAL){
+    
+    gen[typesize] = 0;//gen init
+    genIndex[typesize] = -1;//gen init
+    genpt[typesize] = -1;
+    geneta[typesize] = -20;
+    genphi[typesize] = -20;
+    geny[typesize] = -1;
+    int mGenIdxTk1=-1;
+    int mGenIdxTk2=-1;
+    int bGenIdxTk1=-1;
+    int bGenIdxTk2=-1;
+    int bGenIdxMu1=-1;
+    int bGenIdxMu2=-1;
       
       
-      float BId,MId,tk1Id,tk2Id;
-      //tk1:positive, tk2:negtive
-      if(BInfo_type[j]==4)
-	{
-	  BId = 511;//B0
-	  MId = 313;//K*0
-	  tk1Id = 321;//K+
-	  tk2Id = 211;//pi-
-	}
-      if(BInfo_type[j]==5)
-	{
-	  BId = 511;//B0
-	  MId = 313;//K*0
-	  tk1Id = 211;//pi+
-	  tk2Id = 321;//K-
-	}
+    float BId,MId,tk1Id,tk2Id;
+    //tk1:positive, tk2:negtive
+	BId = 511;//B0
+	MId = 313;//K*0
+	tk1Id = 321;//K+
+	tk2Id = 211;//pi-
       
       int twoTks,kStar,flagkstar=0;
       if(BInfo_type[j]==1 || BInfo_type[j]==2) twoTks=0;
@@ -450,6 +440,50 @@ void fillTree(TVector3* bP, TVector3* bVtx, TLorentzVector* b4P, int j, int type
 	  geny[typesize] = b4P->Rapidity();
 	}
     }
+}
+
+
+bool IsBzeroToJpsiK0starKPi(int mytrk1geninfo,int mypdgtrk1,int mymothertrk1geninfo,int mypdgmothertrk1,int mygrandmothertrk1geninfo,int mypdggrandmothertrk1,
+                   int mytrk2geninfo,int mypdgtrk2,int mymothertrk2geninfo,int mypdgmothertrk2,int mygrandmothertrk2geninfo,int mypdggrandmothertrk2,
+                   int mymuon1geninfo,int mypdgmuon1,int mymothermuon1geninfo,int mypdgmothermuon1,int mygrandmothermuon1geninfo,int mypdggrandmothermuon1, 
+                   int mymuon2geninfo,int mypdgmuon2,int mymothermuon2geninfo,int mypdgmothermuon2,int mygrandmothermuon2geninfo,int mypdggrandmothermuon2){
+  
+  int bGenIdxTk1=-1;
+  int bGenIdxTk2=-1;
+  int bGenIdxMu1=-1;
+  int bGenIdxMu2=-1;
+  int mGenIdxTk1=-1;
+  int mGenIdxTk2=-1;
+    
+  int BmesonId = 511;//
+  int trkpar1Id = 321;//kaon
+  int trkpar2Id = 211;//pion
+  int trkJpsiId = 443;//jpsi
+  int trkk0starId = 313;//phi
+  int trkMuon = 13;//muon
+  
+  bool okTrk1=false;
+  bool okTrk2=false;
+  bool okMuon1=false;
+  bool okMuon2=false;
+  bool okTotal=false;
+  //
+  okTrk1=IsFromBviaresonance(mytrk1geninfo,mypdgtrk1,mymothertrk1geninfo,mypdgmothertrk1,
+                              mygrandmothertrk1geninfo,mypdggrandmothertrk1,trkpar1Id,trkk0starId,BmesonId,bGenIdxTk1);
+  okTrk2=IsFromBviaresonance(mytrk2geninfo,mypdgtrk2,mymothertrk2geninfo,mypdgmothertrk2,
+                              mygrandmothertrk2geninfo,mypdggrandmothertrk2,trkpar2Id,trkk0starId,BmesonId,bGenIdxTk2);                                                   
+  okMuon1=IsFromBviaresonance(mymuon1geninfo,mypdgmuon1,mymothermuon1geninfo,mypdgmothermuon1,
+                              mygrandmothermuon1geninfo,mypdggrandmothermuon1,trkMuon,trkJpsiId,BmesonId,bGenIdxMu1);
+  okMuon2=IsFromBviaresonance(mymuon2geninfo,mypdgmuon2,mymothermuon2geninfo,mypdgmothermuon2,
+                              mygrandmothermuon2geninfo,mypdggrandmothermuon2,trkMuon,trkJpsiId,BmesonId,bGenIdxMu2);
+                              
+  if(okMuon2&&okMuon1&&okTrk1&&okTrk2){
+    if(bGenIdxMu1!=-1 && bGenIdxMu2!=-1 && bGenIdxMu1==bGenIdxMu2 && bGenIdxMu1==bGenIdxTk1 && bGenIdxTk2==bGenIdxTk1){
+      okTotal=true;
+    }
+  }
+  return okTotal;
+  
 }
 
 
