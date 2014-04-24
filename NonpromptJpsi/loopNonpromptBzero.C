b61f71dd2448dc65cf55dbfa2fbb1ff27343b408
gian michele <ginnocen@cern.ch>
2014-04-24 12:29:40 -0400

    upd

--- NonpromptJpsi/loopNonpromptBzero.C
+++ NonpromptJpsi/loopNonpromptBzero.C
@@ -235,8 +235,15 @@ void fillTree(TVector3* bP, TVector3* bVtx, TLorentzVector* b4P, int j, int type
                                     trk2geninfo,pdgtrk2,mothertrk2geninfo,pdgmothertrk2,grandmothertrk2geninfo,pdggrandmothertrk2,
                                     muon1geninfo,pdgmuon1,mothermuon1geninfo,pdgmothermuon1,grandmothermuon1geninfo,pdggrandmothermuon1,
                                     muon2geninfo,pdgmuon2,mothermuon2geninfo,pdgmothermuon2,grandmothermuon2geninfo,pdggrandmothermuon2);
+    bool isbzerotojpsik0starpik=IsBzeroToJpsiK0starPiK(trk1geninfo,pdgtrk1,mothertrk1geninfo,pdgmothertrk1,grandmothertrk1geninfo,pdggrandmothertrk1,
+                                    trk2geninfo,pdgtrk2,mothertrk2geninfo,pdgmothertrk2,grandmothertrk2geninfo,pdggrandmothertrk2,
+                                    muon1geninfo,pdgmuon1,mothermuon1geninfo,pdgmothermuon1,grandmothermuon1geninfo,pdggrandmothermuon1,
+                                    muon2geninfo,pdgmuon2,mothermuon2geninfo,pdgmothermuon2,grandmothermuon2geninfo,pdggrandmothermuon2);
+
+    
     std::cout<<"TOTAL"<<isbzerotojpsik0starkpi<<std::endl;
     genBzeroToJpsiK0starKPi[typesize]=(int)(isbzerotojpsik0starkpi);
+    genBzeroToJpsiK0starPiK[typesize]=(int)(isbzerotojpsik0starpik);
      
   }
 }
@@ -257,6 +264,54 @@ bool IsBzeroToJpsiK0starKPi(int mytrk1geninfo,int mypdgtrk1,int mymothertrk1geni
   int BmesonId = 511;//
   int trkpar1Id = 321;//kaon
   int trkpar2Id = 211;//pion
+  int trkJpsiId = 443;//jpsi
+  int trkk0starId = 313;//phi
+  int trkMuon = 13;//muon
+  
+  bool okTrk1=false;
+  bool okTrk2=false;
+  bool okMuon1=false;
+  bool okMuon2=false;
+  bool okTotal=false;
+  //
+  okTrk1=IsFromBviaresonance(mytrk1geninfo,mypdgtrk1,mymothertrk1geninfo,mypdgmothertrk1,
+                              mygrandmothertrk1geninfo,mypdggrandmothertrk1,trkpar1Id,trkk0starId,BmesonId,bGenIdxTk1);
+  okTrk2=IsFromBviaresonance(mytrk2geninfo,mypdgtrk2,mymothertrk2geninfo,mypdgmothertrk2,
+                              mygrandmothertrk2geninfo,mypdggrandmothertrk2,trkpar2Id,trkk0starId,BmesonId,bGenIdxTk2);                                                   
+  okMuon1=IsFromBviaresonance(mymuon1geninfo,mypdgmuon1,mymothermuon1geninfo,mypdgmothermuon1,
+                              mygrandmothermuon1geninfo,mypdggrandmothermuon1,trkMuon,trkJpsiId,BmesonId,bGenIdxMu1);
+  okMuon2=IsFromBviaresonance(mymuon2geninfo,mypdgmuon2,mymothermuon2geninfo,mypdgmothermuon2,
+                              mygrandmothermuon2geninfo,mypdggrandmothermuon2,trkMuon,trkJpsiId,BmesonId,bGenIdxMu2);
+  
+  //std::cout<<okTrk1<<okTrk2<<okMuon1<<okMuon2<<std::endl;
+  //std::cout<<bGenIdxMu1<<"-"<<bGenIdxMu2<<"-"<<bGenIdxTk1<<"-"<<bGenIdxTk2<<std::endl;
+  //  std::cout<<"//////"<<std::endl;
+
+  if(okMuon2&&okMuon1&&okTrk1&&okTrk2){
+    if(bGenIdxMu1!=-1 && bGenIdxMu2!=-1 && bGenIdxMu1==bGenIdxMu2 && bGenIdxMu1==bGenIdxTk1 && bGenIdxTk2==bGenIdxTk1){
+      okTotal=true;
+    }
+  }
+  //std::cout<<"TOTAL"<<okTotal<<std::endl;
+  return okTotal;
+  
+}
+
+bool IsBzeroToJpsiK0starPiK(int mytrk1geninfo,int mypdgtrk1,int mymothertrk1geninfo,int mypdgmothertrk1,int mygrandmothertrk1geninfo,int mypdggrandmothertrk1,
+                   int mytrk2geninfo,int mypdgtrk2,int mymothertrk2geninfo,int mypdgmothertrk2,int mygrandmothertrk2geninfo,int mypdggrandmothertrk2,
+                   int mymuon1geninfo,int mypdgmuon1,int mymothermuon1geninfo,int mypdgmothermuon1,int mygrandmothermuon1geninfo,int mypdggrandmothermuon1, 
+                   int mymuon2geninfo,int mypdgmuon2,int mymothermuon2geninfo,int mypdgmothermuon2,int mygrandmothermuon2geninfo,int mypdggrandmothermuon2){
+  
+  int bGenIdxTk1=-1;
+  int bGenIdxTk2=-1;
+  int bGenIdxMu1=-1;
+  int bGenIdxMu2=-1;
+  int mGenIdxTk1=-1;
+  int mGenIdxTk2=-1;
+    
+  int BmesonId = 511;//
+  int trkpar1Id = 211;//kaon
+  int trkpar2Id = 321;//pion
   int trkJpsiId = 443;//jpsi
   int trkk0starId = 313;//phi
   int trkMuon = 13;//muon
