b5b478a2e4760b754994b99e6c63db24c3d7b562
gian michele <ginnocen@cern.ch>
2014-04-25 09:06:16 -0400

    upd

--- ComparisonMCData/LoopMCDataSignal.C
+++ ComparisonMCData/LoopMCDataSignal.C
@@ -3,7 +3,8 @@
 TString inputdata="/export/d00/scratch/jwang/nt_20140418_PAMuon_HIRun2013_PromptrecoAndRereco_v1_MuonMatching_EvtBase_skim.root";
 TString inputmc="/export/d00/scratch/jwang/nt_BoostedMC_20140418_Kp_TriggerMatchingMuon_EvtBase_skim.root";
 
-TString cut="(HLT_PAMu3_v1)&&abs(mumumass-3.096916)<0.15&&chi2cl>0.0054&&(d0)/d0Err>3.3&&cos(dtheta)>-0.53&&TMath::Abs((trk1Dxy)/trk1D0Err)>1.9&&mass>5.24&&mass<5.31";
+//TString cut="(HLT_PAMu3_v1)&&abs(mumumass-3.096916)<0.15&&chi2cl>0.0054&&(d0)/d0Err>3.3&&cos(dtheta)>-0.53&&TMath::Abs((trk1Dxy)/trk1D0Err)>1.9&&mass>5.24&&mass<5.31";
+TString cut="(HLT_PAMu3_v1)&&abs(mumumass-3.096916)<0.15&&mass>5.24&&mass<5.31&&isbestchi2&&trk1Pt>0.9&&chi2cl>1.32e-02&&(d0/d0Err)>3.41&&cos(dtheta)>-3.46e01";
 
 TString seldata=Form("abs(y+0.465)<1.93&&%s&&pt>10.&&pt<60.",cut.Data());
 TString selmc="1";//Form("abs(y+0.465)<1.93&&%s&&gen!=22233&&pt>10.&&pt<60.",cut.Data());
