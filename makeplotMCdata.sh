24b9ab5b63918fba23a3555ab22cc3cce5a7913d
gian michele <ginnocen@cern.ch>
2014-04-30 10:49:15 -0400

    upd

--- makeplotMCdata.sh
+++ makeplotMCdata.sh
@@ -1,21 +1,21 @@
 
 
 time root -b > an.log 2>&1 <<EOI
-.x dataMC.C+("d0/d0Err","d0/#sigma(d0)","d0D0errbestchi",40,0,200,1)
+.x dataMC.C+("d0/d0Err","d0/#sigma(d0)","d0D0erroptimal",40,0,200,1)
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x dataMC.C+("chi2cl","Vertex #chi^{2} Probability","ProbChi2bestchi",20,0,0)
+.x dataMC.C+("chi2cl","Vertex #chi^{2} Probability","ProbChi2optimal",20,0,0)
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x dataMC.C+("cos(dtheta)","cos(#Delta#theta)","cosdthetabestchi",20,-1,0)
+.x dataMC.C+("cos(dtheta)","cos(#Delta#theta)","cosdthetaoptimal",20,-1,0)
 .q
 EOI
 
 time root -b > an.log 2>&1 <<EOI
-.x dataMC.C+("abs(trk1Dxy/trk1D0Err)","|trk1Dxy/trk1D0Err|","trk1DxyDerrbestchi",40,0,200,1)
+.x dataMC.C+("abs(trk1Dxy/trk1D0Err)","|trk1Dxy/trk1D0Err|","trk1DxyDerroptimal",40,0,200,1)
 .q
 EOI
