74482e54370dc9e796e7ace3631feb0a87d7b3f6
gian michele <ginnocen@cern.ch>
2014-04-30 06:55:23 -0400

    update

--- makeplotMCdata.sh
+++ makeplotMCdata.sh
@@ -1,9 +1,21 @@
 
 
-time aliroot -b > an.log 2>&1 <<EOI
-.x dataMC.C+("d0/d0Err","d0/#sigma(d0)","d0D0err",40,0,200,1)
-.x dataMC.C+("chi2cl","Vertex #chi^{2} Probability","ProbChi2",20,0,1)
-.x dataMC.C+("cos(dtheta)","cos(#Delta#theta)","cosdtheta",20,-1,1)
-.x dataMC.C+("abs(trk1Dxy/trk1D0Err)","|trk1Dxy/trk1D0Err|","trk1DxyDerr",40,0,200,1)
+time root -b > an.log 2>&1 <<EOI
+.x dataMC.C+("d0/d0Err","d0/#sigma(d0)","d0D0errbestchi",40,0,200,1)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x dataMC.C+("chi2cl","Vertex #chi^{2} Probability","ProbChi2bestchi",20,0,1)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x dataMC.C+("cos(dtheta)","cos(#Delta#theta)","cosdthetabestchi",20,-1,1)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x dataMC.C+("abs(trk1Dxy/trk1D0Err)","|trk1Dxy/trk1D0Err|","trk1DxyDerrbestchi",40,0,200,1)
 .q
 EOI
