5139c5f804d38a3dfc43e23b3d335d5f30f496a2
gian michele innocenti <ginnocen@cern.ch>
2014-05-04 01:32:55 +0200

    update

--- FinalSystCut/UncertaintyCut/makestudy.sh
+++ FinalSystCut/UncertaintyCut/makestudy.sh
@@ -1,5 +1,20 @@
 
 time root -b > an.log 2>&1 <<EOI
-.x StudyUncCut.C+(2)
+.x StudyUncCut.C+(5)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCut.C+(6)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCut.C+(7)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCut.C+(8)
 .q
 EOI
