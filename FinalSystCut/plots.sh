43ab47046569c97488de537f08f442191f1c22e6
gian michele innocenti <ginnocen@cern.ch>
2014-05-03 12:08:59 +0200

    add plotting macro

---
+++ FinalSystCut/plots.sh
@@ -0,0 +1,22 @@
+
+time root -b > an.log 2>&1 <<EOI
+.x ComparedNdcutNew.C(1,8)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x ComparedNdcutNew.C(2,8)
+.q
+EOI
+
+
+time root -b > an.log 2>&1 <<EOI
+.x ComparedNdcutNew.C(3,8)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x ComparedNdcutNew.C(4,8)
+.q
+EOI
+
