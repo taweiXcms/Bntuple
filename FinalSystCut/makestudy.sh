92b52dedf25476db1b4aab40c64a566937b09b3f
gian michele innocenti <ginnocen@cern.ch>
2014-05-03 02:09:18 +0200

    update

--- FinalSystCut/makestudy.sh
+++ FinalSystCut/makestudy.sh
@@ -1,20 +1,30 @@
 
 time root -b > an.log 2>&1 <<EOI
+.x SystStudydNdpt.C+(1)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
 .x SystStudydNdpt.C+(2)
 .q
 EOI
 
-#time root -b > an.log 2>&1 <<EOI
-#.x SystStudydNdpt.C+(4)
-#.q
-#EOI
+time root -b > an.log 2>&1 <<EOI
+.x SystStudydNdpt.C+(3)
+.q
+EOI
 
-#time root -b > an.log 2>&1 <<EOI
-#.x SystStudydNdpt.C+(2)
-#.q
-#EOI
+time root -b > an.log 2>&1 <<EOI
+.x SystStudydNdpt.C+(4)
+.q
+EOI
 
-#time root -b > an.log 2>&1 <<EOI
-#.x SystStudy.C+(1)
-#.q
-#EOI
+time root -b > an.log 2>&1 <<EOI
+.x SystStudydNdpt.C+(5)
+.q
+EOI
+
+time root -b > an.log 2>&1 <<EOI
+.x SystStudydNdpt.C+(6)
+.q
+EOI
