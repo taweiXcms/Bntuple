577d4efdc874016bf7939622f0d3e5dd5add0d18
gian michele innocenti <ginnocen@cern.ch>
2014-05-04 14:02:44 +0200

    new steering macro

---
+++ FinalSystCut/UncertaintyCut/makestudyBzero.sh
@@ -0,0 +1,23 @@
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCutBzero.C+(1)
+.q
+EOI
+
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCutBzero.C+(2)
+.q
+EOI
+
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCutBzero.C+(3)
+.q
+EOI
+
+
+time root -b > an.log 2>&1 <<EOI
+.x StudyUncCutBzero.C+(4)
+.q
+EOI
