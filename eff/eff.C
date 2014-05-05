f3dcfb7f3e3bdce15d0a0e89acaa4ac23d9a940e
Ta Wei <ta-wei.wang@cern.ch>
2014-05-05 02:26:32 -0400

    eff up

--- eff/eff.C
+++ eff/eff.C
@@ -23,91 +23,267 @@
 #define JPSI_MASS   3.096916
 #define PSI2S_MASS  3.686109
 
+float lumi1 = 20.8;
+float lumi2 = 14.0;
 using namespace std;
-
 TString inputmc;
+TString inputmc2;
 TString cut;
 TString selmc;
+TString selmc2;
 TString foutname;
 TString ntname;
 int MCflag = 1;
-TString fig_folder = "fig_eff";
 //TString fig_folder = "fig_eff_fine";
+TString fig_folder = "dummy";
+void AcceptedCand(TTree *ntGen, TH1F* gen_bsig_pt, TH1F* gen_bsig_y, bool IsPpb);
+void DrawMyEff(TCanvas* c, TEfficiency* eff, TString axis);
 
 void eff(){
   if(MCflag==1){
     inputmc="/export/d00/scratch/jwang/nt_BoostedMC_20140427_Kp_TriggerMatchingMuon_EvtBase_skim.root";
+    inputmc2="/net/hidsk0001/d00/scratch/jwang/nt_BoostedMC_20140501_Hijing_PPb502_MinimumBias_HIJINGemb_BuJpsiKp_5TeV_boostPbp.root";
     cut="(HLT_PAMu3_v1)&&abs(mumumass-3.096916)<0.15&&mass>5&&mass<6&& isbestchi2&&trk1Pt>0.9&&chi2cl>1.32e-02&&(d0/d0Err)>3.41&&cos(dtheta)>-3.46e01";
     selmc=Form("abs(y+0.465)<1.93&&gen==23333&&pt>10&&pt<60&&%s",cut.Data());
+    selmc2=Form("abs(y-0.465)<1.93&&gen==23333&&pt>10&&pt<60&&%s",cut.Data());
     foutname = "fout_kp.root";
     ntname="ntKp";
   }
   if(MCflag==2){
     inputmc="/export/d00/scratch/jwang/nt_BoostedMC_20140427_Kstar_TriggerMatchingMuon_EvtBase_skim.root";
+    inputmc2="/net/hidsk0001/d00/scratch/jwang/nt_BoostedMC_20140501_Hijing_PPb502_MinimumBias_HIJINGemb_BdJpsiKstar_5TeV_boostPbp.root";
     cut="(HLT_PAMu3_v1)&&abs(mumumass-3.096916)<0.15&&mass>5&&mass<6&& isbestchi2&&trk1Pt>0.7&&trk2Pt>0.7&&chi2cl>1.65e-01&&(d0/d0Err)>4.16&&cos(dtheta)>7.50e-01&&abs(tktkmass-0.89594)<2.33e-01";
 //    selmc=Form("abs(y+0.465)<1.93&&(gen==23333||gen==41000)&&pt>10&&pt<60&&%s",cut_kpi.Data());
     selmc=Form("abs(y+0.465)<1.93&&gen==23333&&pt>10&&pt<60&&%s",cut.Data());
+    selmc2=Form("abs(y-0.465)<1.93&&gen==23333&&pt>10&&pt<60&&%s",cut.Data());
     foutname = "fout_kstar.root";
     ntname="ntKstar";
   }
   if(MCflag==3){
     inputmc="/export/d00/scratch/jwang/nt_BoostedMC_20140427_Phi_TriggerMatchingMuon_EvtBase_skim.root";
+    inputmc2="/net/hidsk0001/d00/scratch/jwang/nt_BoostedMC_20140501_Hijing_PPb502_MinimumBias_HIJINGemb_BsJpsiPhi_5TeV_boostPbp.root";
     cut="(HLT_PAMu3_v1)&&abs(mumumass-3.096916)<0.15&&mass>5&&mass<6&& isbestchi2&&trk1Pt>0.7&&trk2Pt>0.7&& chi2cl>3.71e-02&&(d0/d0Err)>3.37&&cos(dtheta)>2.60e-01&&abs(tktkmass-1.019455)<1.55e-02";
     selmc=Form("abs(y+0.465)<1.93&&gen==23333&&pt>10&&pt<60&&%s",cut.Data());
+    selmc2=Form("abs(y-0.465)<1.93&&gen==23333&&pt>10&&pt<60&&%s",cut.Data());
     foutname = "fout_phi.root";
     ntname="ntphi";
   }
   TFile *infMC = new TFile(inputmc.Data());
+  TFile *infMC2 = new TFile(inputmc2.Data());
   TTree *ntGen = (TTree*)infMC->Get("ntGen");
+  TTree *ntGen2 = (TTree*)infMC2->Get("ntGen");
   TTree *ntMC = (TTree*)infMC->Get(ntname.Data());
+  TTree *ntMC2 = (TTree*)infMC2->Get(ntname.Data());
   TFile *fout = new TFile(foutname.Data(),"recreate");
 
-  const int nBins = 6;
-  double ptBins[nBins+1] = {5,10,15,20,25,30,60};
+  const int nBins = 5;
+  double ptBins[nBins+1] = {10,15,20,25,30,60};
+//  const int nBins = 3;
+//  double ptBins[nBins+1] = {10,15,20,60};
+//  const int nBins = 1;
+//  double ptBins[nBins+1] = {10,60};
   const int nEtaBins = 4;
-  double yBins[nEtaBins+1] = {-2.395, -1.47, -0.47, 0.53, 1.465};
+//  double yBins[nEtaBins+1] = {-2.395, -1.47, -0.47, 0.53, 1.465};
+  double yBins[nEtaBins+1] = {-1.93, -1., 0, 1., 1.93};
+
   TH1F* reco_bsig_pt = new TH1F("reco_bsig_pt", "", nBins,ptBins);
   TH1F* reco_bsig_y = new TH1F("reco_bsig_y", "", nEtaBins,yBins);
   TH1F* gen_bsig_pt = new TH1F("gen_bsig_pt", "", nBins,ptBins);
   TH1F* gen_bsig_y = new TH1F("gen_bsig_y", "", nEtaBins,yBins);
-//  TH1F* reco_bsig_pt = new TH1F("reco_bsig_pt", "", 50, ptBins[0], ptBins[nBins]);
-//  TH1F* reco_bsig_y = new TH1F("reco_bsig_y", "", 50, yBins[0], yBins[nEtaBins]);
-//  TH1F* gen_bsig_pt = new TH1F("gen_bsig_pt", "", 50, ptBins[0], ptBins[nBins]);
-//  TH1F* gen_bsig_y = new TH1F("gen_bsig_y", "", 50, yBins[0], yBins[nEtaBins]);
+  TH1F* reco_bsig_pt2 = new TH1F("reco_bsig_pt2", "", nBins,ptBins);
+  TH1F* reco_bsig_y2 = new TH1F("reco_bsig_y2", "", nEtaBins,yBins);
+  TH1F* gen_bsig_pt2 = new TH1F("gen_bsig_pt2", "", nBins,ptBins);
+  TH1F* gen_bsig_y2 = new TH1F("gen_bsig_y2", "", nEtaBins,yBins);
+
+/*
+  TH1F* reco_bsig_pt = new TH1F("reco_bsig_pt", "", 50, ptBins[0], ptBins[nBins]);
+  TH1F* reco_bsig_y = new TH1F("reco_bsig_y", "", 50, yBins[0], yBins[nEtaBins]);
+  TH1F* gen_bsig_pt = new TH1F("gen_bsig_pt", "", 50, ptBins[0], ptBins[nBins]);
+  TH1F* gen_bsig_y = new TH1F("gen_bsig_y", "", 50, yBins[0], yBins[nEtaBins]);
+  TH1F* reco_bsig_pt2 = new TH1F("reco_bsig_pt2", "", 20, ptBins[0], ptBins[nBins]);
+  TH1F* reco_bsig_y2 = new TH1F("reco_bsig_y2", "", 20, yBins[0], yBins[nEtaBins]);
+  TH1F* gen_bsig_pt2 = new TH1F("gen_bsig_pt2", "", 20, ptBins[0], ptBins[nBins]);
+  TH1F* gen_bsig_y2 = new TH1F("gen_bsig_y2", "", 20, yBins[0], yBins[nEtaBins]);
+*/
 
 //  TH2F* sct_pt_y = new TH2F("sct_pt_y","",nEtaBins, yBins, nBins, ptBins);
-  TH2F* sct_pt_y = new TH2F("sct_pt_y","",50,-2.395, 1.465, 50, 10, 60);
+//  TH2F* sct_pt_y2 = new TH2F("sct_pt_y2","",nEtaBins, yBins, nBins, ptBins);
+  TH2F* sct_pt_y = new TH2F("sct_pt_y","",20,-1.93, 1.93, 20, 10, 60);
+  TH2F* sct_pt_y2 = new TH2F("sct_pt_y2","",20,-1.93, 1.93, 20, 10, 60);
 
   reco_bsig_pt->SetMinimum(0);
   reco_bsig_y->SetMinimum(0);
   gen_bsig_pt->SetMinimum(0);
   gen_bsig_y->SetMinimum(0);
+  reco_bsig_pt2->SetMinimum(0);
+  reco_bsig_y2->SetMinimum(0);
+  gen_bsig_pt2->SetMinimum(0);
+  gen_bsig_y2->SetMinimum(0);
+  sct_pt_y->SetMinimum(0);
+  sct_pt_y2->SetMinimum(0);
 
-  int nevents_total = ntGen->GetEntries();
+  int nevents_total = ntMC->GetEntries();
 //  nevents_total = 10000;
   ntMC->Project("reco_bsig_pt","pt",selmc.Data(),"",nevents_total);
-  ntMC->Project("reco_bsig_y","y",selmc.Data(),"",nevents_total);
-  ntMC->Project("sct_pt_y","pt:y",selmc.Data(),"",nevents_total);
+  ntMC->Project("reco_bsig_y","(y+0.465)",selmc.Data(),"",nevents_total);
+  ntMC->Project("sct_pt_y","pt:(y+0.465)",selmc.Data(),"",nevents_total);
+  ntMC2->Project("reco_bsig_pt2","pt",selmc2.Data(),"",nevents_total);
+  ntMC2->Project("reco_bsig_y2","(y-0.465)",selmc2.Data(),"",nevents_total);
+  ntMC2->Project("sct_pt_y2","pt:(y-0.465)",selmc2.Data(),"",nevents_total);
 
   TCanvas*c3 = new TCanvas("c3", "", 200, 10, 1000, 800);
   c3->cd();
   sct_pt_y->GetYaxis()->SetTitle("pt[GeV]");                      
-  sct_pt_y->GetXaxis()->SetTitle("y_{Lab}"); 
+  sct_pt_y->GetXaxis()->SetTitle("y_{CM}"); 
   sct_pt_y->GetYaxis()->CenterTitle(); 
   sct_pt_y->Draw("COL Z");
+  TCanvas*c4 = new TCanvas("c4", "", 200, 10, 1000, 800);
+  c4->cd();
+  sct_pt_y2->GetYaxis()->SetTitle("pt[GeV]");                      
+  sct_pt_y2->GetXaxis()->SetTitle("y_{CM}"); 
+  sct_pt_y2->GetYaxis()->CenterTitle(); 
+  sct_pt_y2->Draw("COL Z");
   if(MCflag == 1){
       sct_pt_y->SetTitle("Reco Can. K+ channel");                                                                                                                                                    
-      c3->SaveAs(fig_folder+"/kp_sct.pdf");
+      c3->SaveAs(fig_folder+"_pPb/kp_sct.pdf");
+      sct_pt_y2->SetTitle("Reco Can. K+ channel");                                                                                                                                                    
+      c4->SaveAs(fig_folder+"_Pbp/kp_sct.pdf");
   }
   if(MCflag == 2){
       sct_pt_y->SetTitle("Reco Can. K* channel");
-      c3->SaveAs(fig_folder+"/kstar_sct.pdf");
+      c3->SaveAs(fig_folder+"_pPb/kstar_sct.pdf");
+      sct_pt_y2->SetTitle("Reco Can. K* channel");
+      c4->SaveAs(fig_folder+"_Pbp/kstar_sct.pdf");
   }
   if(MCflag == 3){
       sct_pt_y->SetTitle("Reco Can.  #phi channel");
-      c3->SaveAs(fig_folder+"/phi_sct.pdf");
+      c3->SaveAs(fig_folder+"_pPb/phi_sct.pdf");
+      sct_pt_y2->SetTitle("Reco Can.  #phi channel");
+      c4->SaveAs(fig_folder+"_Pbp/phi_sct.pdf");
   }
- return;
+  //return;
+
+  AcceptedCand(ntGen, gen_bsig_pt, gen_bsig_y, true);
+  AcceptedCand(ntGen2,gen_bsig_pt2, gen_bsig_y2, false);
+
+  ////PLOTTING////
+  TEfficiency *reco_gen_eff_pt = new TEfficiency(*reco_bsig_pt, *gen_bsig_pt);
+  TEfficiency *reco_gen_eff_y = new TEfficiency(*reco_bsig_y, *gen_bsig_y);
+  TEfficiency *reco_gen_eff_pt2 = new TEfficiency(*reco_bsig_pt2, *gen_bsig_pt2);
+  TEfficiency *reco_gen_eff_y2 = new TEfficiency(*reco_bsig_y2, *gen_bsig_y2);
+  //reco_gen_eff_pt->SetConfidenceLevel(0.95);
+  //reco_gen_eff_y->SetConfidenceLevel(0.95);
+
+/*
+reco_gen_eff_y2->SetWeight(200);
+reco_gen_eff_pt_merge = (TEfficiency*)reco_gen_eff_pt->Clone();
+reco_gen_eff_pt_merge->Add(*reco_gen_eff_pt2);
+//reco_gen_eff_y_merge = (TEfficiency*)reco_gen_eff_y->Clone();
+//reco_gen_eff_y_merge->Add(*reco_gen_eff_y2);
+reco_gen_eff_y_merge = (TEfficiency*)reco_gen_eff_y2->Clone();
+reco_gen_eff_y_merge->SetWeight(200.);
+cout<<reco_gen_eff_y_merge->GetWeight()<<endl;
+reco_gen_eff_y->SetWeight(0.00000001);
+//reco_gen_eff_y_merge->Add(*reco_gen_eff_y);
+*reco_gen_eff_y_merge += (*reco_gen_eff_y);
+cout<<reco_gen_eff_y_merge->GetWeight()<<endl;
+*/
+
+  TCanvas*c1 = new TCanvas("c1", "", 200, 10, 1000, 800);
+  TCanvas*c2 = new TCanvas("c2", "", 200, 10, 1000, 800);
+  TCanvas*c5 = new TCanvas("c5", "", 200, 10, 1000, 800);
+  TCanvas*c6 = new TCanvas("c6", "", 200, 10, 1000, 800);
+  if(MCflag == 1){
+      reco_gen_eff_pt->SetTitle("eff base, K+ channel");
+      reco_gen_eff_y->SetTitle("eff base, K+ channel");
+      reco_gen_eff_pt2->SetTitle("eff base, K+ channel");
+      reco_gen_eff_y2->SetTitle("eff base, K+ channel");
+  }
+  if(MCflag == 2){
+      reco_gen_eff_pt->SetTitle("eff base, K* channel");
+      reco_gen_eff_y->SetTitle("eff base, K* channel");
+      reco_gen_eff_pt2->SetTitle("eff base, K* channel");
+      reco_gen_eff_y2->SetTitle("eff base, K* channel");
+  }
+  if(MCflag == 3){
+      reco_gen_eff_pt->SetTitle("eff base, #phi channel");
+      reco_gen_eff_y->SetTitle("eff base, #phi channel");
+      reco_gen_eff_pt2->SetTitle("eff base, #phi channel");
+      reco_gen_eff_y2->SetTitle("eff base, #phi channel");
+  }
+
+  cout<<"pt 1-------------"<<endl;
+  for(int i = 0; i < reco_bsig_pt->GetNbinsX(); i ++){
+//      printf("bin content pt: %.0f, %.0f\n", reco_bsig_pt->GetBinContent(i+1), gen_bsig_pt->GetBinContent(i+1));
+      printf("%.3f$\\pm$%.3f & ", reco_gen_eff_pt->GetEfficiency(i+1), max(reco_gen_eff_pt->GetEfficiencyErrorUp(i+1), reco_gen_eff_pt->GetEfficiencyErrorLow(i+1)));
+  }
+  cout<<endl;
+  cout<<"y 1-------------"<<endl;
+  for(int i = 0; i < reco_bsig_y->GetNbinsX(); i ++){
+//      printf("bincontent y: %.0f, %.0f\n", reco_bsig_y->GetBinContent(i+1), gen_bsig_y->GetBinContent(i+1));
+      printf("%.3f$\\pm$%.3f & ", reco_gen_eff_y->GetEfficiency(i+1), max(reco_gen_eff_y->GetEfficiencyErrorUp(i+1), reco_gen_eff_y->GetEfficiencyErrorLow(i+1)));
+  }
+  cout<<endl;
+  cout<<"pt 2-------------"<<endl;
+  for(int i = 0; i < reco_bsig_pt2->GetNbinsX(); i ++){
+//      printf("bin content pt: %.0f, %.0f\n", reco_bsig_pt2->GetBinContent(i+1), gen_bsig_pt2->GetBinContent(i+1));
+      printf("%.3f$\\pm$%.3f & ", reco_gen_eff_pt2->GetEfficiency(i+1), max(reco_gen_eff_pt2->GetEfficiencyErrorUp(i+1), reco_gen_eff_pt2->GetEfficiencyErrorLow(i+1)));
+  }
+  cout<<endl;
+  cout<<"y 2-------------"<<endl;
+  for(int i = 0; i < reco_bsig_y2->GetNbinsX(); i ++){
+//      printf("bincontent y: %.0f, %.0f\n", reco_bsig_y2->GetBinContent(i+1), gen_bsig_y2->GetBinContent(i+1));
+      printf("%.3f$\\pm$%.3f & ", reco_gen_eff_y2->GetEfficiency(i+1), max(reco_gen_eff_y2->GetEfficiencyErrorUp(i+1), reco_gen_eff_y2->GetEfficiencyErrorLow(i+1)));
+  }
+  cout<<endl;
+
+  float _lumi1 = lumi1/(lumi1+lumi2);
+  float _lumi2 = lumi2/(lumi1+lumi2);
+  cout<<"sum pt-------------"<<endl;
+  for(int i = 0; i < reco_bsig_pt->GetNbinsX(); i ++){
+      printf("%.3f$\\pm$%.3f & ", _lumi1*reco_gen_eff_pt->GetEfficiency(i+1)+_lumi2*reco_gen_eff_pt2->GetEfficiency(i+1),
+      max(sqrt(_lumi1*pow(reco_gen_eff_pt->GetEfficiencyErrorUp(i+1),2) + _lumi2*pow(reco_gen_eff_pt2->GetEfficiencyErrorUp(i+1),2)), 
+          sqrt(_lumi1*pow(reco_gen_eff_pt->GetEfficiencyErrorLow(i+1),2) + _lumi2*pow(reco_gen_eff_pt2->GetEfficiencyErrorLow(i+1),2))));
+  }
+  cout<<endl;
+  cout<<"sum y-------------"<<endl;
+  for(int i = 0; i < reco_bsig_y->GetNbinsX(); i ++){
+      printf("%.3f$\\pm$%.3f & ", _lumi1*reco_gen_eff_y->GetEfficiency(i+1)+_lumi2*reco_gen_eff_y2->GetEfficiency(i+1),
+      max(sqrt(_lumi1*pow(reco_gen_eff_y->GetEfficiencyErrorUp(i+1),2) + _lumi2*pow(reco_gen_eff_y2->GetEfficiencyErrorUp(i+1),2)), 
+          sqrt(_lumi1*pow(reco_gen_eff_y->GetEfficiencyErrorLow(i+1),2) + _lumi2*pow(reco_gen_eff_y2->GetEfficiencyErrorLow(i+1),2))));
+  }
+  cout<<endl;
+
+  DrawMyEff(c1, reco_gen_eff_pt, "pt[GeV]");
+  DrawMyEff(c2, reco_gen_eff_y, "y_{CM}");
+  DrawMyEff(c5, reco_gen_eff_pt2, "pt[GeV]");
+  DrawMyEff(c6, reco_gen_eff_y2, "y_{CM}");
+
+  fout->Write();
+  fout->Close();
+  if(MCflag == 1){
+      c1->SaveAs(fig_folder+"_pPb/kp_pt_eff.pdf");
+      c2->SaveAs(fig_folder+"_pPb/kp_y_eff.pdf");
+      c5->SaveAs(fig_folder+"_Pbp/kp_pt_eff.pdf");
+      c6->SaveAs(fig_folder+"_Pbp/kp_y_eff.pdf");
+  }
+  if(MCflag == 2){
+      c1->SaveAs(fig_folder+"_pPb/kstar_pt_eff.pdf");
+      c2->SaveAs(fig_folder+"_pPb/kstar_y_eff.pdf");
+      c5->SaveAs(fig_folder+"_Pbp/kstar_pt_eff.pdf");
+      c6->SaveAs(fig_folder+"_Pbp/kstar_y_eff.pdf");
+  }
+  if(MCflag == 3){
+      c1->SaveAs(fig_folder+"_pPb/phi_pt_eff.pdf");
+      c2->SaveAs(fig_folder+"_pPb/phi_y_eff.pdf");
+      c5->SaveAs(fig_folder+"_Pbp/phi_pt_eff.pdf");
+      c6->SaveAs(fig_folder+"_Pbp/phi_y_eff.pdf");
+  }
+  ////PLOTING////
+}
+
+void AcceptedCand(TTree *ntGen, TH1F* gen_bsig_pt, TH1F* gen_bsig_y, bool IsPpb){
+  int nevents_total = ntGen->GetEntries();
   Int_t size;
   Float_t y[MAX_GEN];
   Float_t eta[MAX_GEN];
@@ -168,15 +344,20 @@ void eff(){
         mu1.SetPtEtaPhiM(mu1pt[c], mu1eta[c], mu1phi[c], MUON_MASS);
         mu2.SetPtEtaPhiM(mu2pt[c], mu2eta[c], mu2phi[c], MUON_MASS);
         if(MCflag==1){
+          if(tk1pt[c] < 0.9) continue;
           tk1.SetPtEtaPhiM(tk1pt[c], tk1eta[c], tk1phi[c], KAON_MASS);
           B = mu1+mu2+tk1;
         }
         if(MCflag==2){
+          if(tk1pt[c] < 0.7) continue;
+          if(tk2pt[c] < 0.7) continue;
           tk1.SetPtEtaPhiM(tk1pt[c], tk1eta[c], tk1phi[c], KAON_MASS);
           tk2.SetPtEtaPhiM(tk2pt[c], tk2eta[c], tk2phi[c], PION_MASS);
           B = mu1+mu2+tk1+tk2;
         }
         if(MCflag==3){
+          if(tk1pt[c] < 0.7) continue;
+          if(tk2pt[c] < 0.7) continue;
           tk1.SetPtEtaPhiM(tk1pt[c], tk1eta[c], tk1phi[c], KAON_MASS);
           tk2.SetPtEtaPhiM(tk2pt[c], tk2eta[c], tk2phi[c], KAON_MASS);
           B = mu1+mu2+tk1+tk2;
@@ -184,80 +365,31 @@ void eff(){
 		//cout<<B.Mag()<<endl;
         if(B.Mag() < 4.95 || B.Mag() > 5.55) continue;
         
-        if(pt[c]>10 && pt[c]<60 && abs(y[c]+0.465)<1.93){
-          gen_bsig_pt->Fill(pt[c]);
-          gen_bsig_y->Fill(y[c]);
+        if(pt[c]>10 && pt[c]<60){
+          if(IsPpb && abs(y[c]+0.465)<1.93){
+            gen_bsig_pt->Fill(pt[c]);
+            gen_bsig_y->Fill(y[c]+0.465);
+          }
+          if(!IsPpb && abs(y[c]-0.465)<1.93){
+            gen_bsig_pt->Fill(pt[c]);
+            gen_bsig_y->Fill(y[c]-0.465);
+          }
         }
       }
     }
   }
-
-  ////PLOTTING////
-  TEfficiency *reco_gen_eff_pt = new TEfficiency(*reco_bsig_pt, *gen_bsig_pt);
-  TEfficiency *reco_gen_eff_y = new TEfficiency(*reco_bsig_y, *gen_bsig_y);
-  TCanvas*c1 = new TCanvas("c1", "", 200, 10, 1000, 800);
-  TCanvas*c2 = new TCanvas("c2", "", 200, 10, 1000, 800);
-  if(MCflag == 1){
-      reco_gen_eff_pt->SetTitle("eff base, K+ channel");
-      reco_gen_eff_y->SetTitle("eff base, K+ channel");
-  }
-  if(MCflag == 2){
-      reco_gen_eff_pt->SetTitle("eff base, K* channel");
-      reco_gen_eff_y->SetTitle("eff base, K* channel");
-  }
-  if(MCflag == 3){
-      reco_gen_eff_pt->SetTitle("eff base, #phi channel");
-      reco_gen_eff_y->SetTitle("eff base, #phi channel");
-  }
-  for(int i = 0; i < reco_bsig_pt->GetNbinsX(); i ++){
-      printf("bin content pt: %.0f, %.0f\n", reco_bsig_pt->GetBinContent(i+1), gen_bsig_pt->GetBinContent(i+1));
-  }
-
-  c1->cd();
-  reco_gen_eff_pt->SetFillColor(2);
-  reco_gen_eff_pt->SetFillStyle(3001);
-  reco_gen_eff_pt->Draw("");
-  c1->Update();
-  reco_gen_eff_pt->GetPaintedGraph()->GetXaxis()->SetTitle("pt[GeV]");
-  reco_gen_eff_pt->GetPaintedGraph()->SetMaximum(1);
-  reco_gen_eff_pt->GetPaintedGraph()->SetMinimum(0);
-  reco_gen_eff_pt->Draw("a2");
-  reco_gen_eff_pt->Draw("same");
-  reco_gen_eff_pt->Write();
-  c1->Write();
-
-  for(int i = 0; i < reco_bsig_y->GetNbinsX(); i ++){
-      printf("bincontent y: %.0f, %.0f\n", reco_bsig_y->GetBinContent(i+1), gen_bsig_y->GetBinContent(i+1));
-  }
-
-  c2->cd();
-  reco_gen_eff_y->SetFillColor(2);
-  reco_gen_eff_y->SetFillStyle(3001);
-  reco_gen_eff_y->Draw("");
-  c2->Update();
-  reco_gen_eff_y->GetPaintedGraph()->GetXaxis()->SetTitle("y_{Lab}");
-  reco_gen_eff_y->GetPaintedGraph()->SetMaximum(1);
-  reco_gen_eff_y->GetPaintedGraph()->SetMinimum(0);
-  reco_gen_eff_y->Draw("a2");
-  reco_gen_eff_y->Draw("same");
-  reco_gen_eff_y->Write();
-  c2->Update();
-  c2->Write();
-
-  fout->Write();
-  fout->Close();
-  if(MCflag == 1){
-      c1->SaveAs(fig_folder+"/kp_pt_eff.pdf");
-      c2->SaveAs(fig_folder+"/kp_y_eff.pdf");
-  }
-  if(MCflag == 2){
-      c1->SaveAs(fig_folder+"/kstar_pt_eff.pdf");
-      c2->SaveAs(fig_folder+"/kstar_y_eff.pdf");
-  }
-  if(MCflag == 3){
-      c1->SaveAs(fig_folder+"/phi_pt_eff.pdf");
-      c2->SaveAs(fig_folder+"/phi_y_eff.pdf");
-  }
-
-  ////PLOTING////
+}
+void DrawMyEff(TCanvas* c, TEfficiency* eff, TString axis){
+  c->cd();
+  eff->SetFillColor(2);
+  eff->SetFillStyle(3001);
+  eff->Draw("");
+  c->Update();
+  eff->GetPaintedGraph()->GetXaxis()->SetTitle(axis);
+  eff->GetPaintedGraph()->SetMaximum(1);
+  eff->GetPaintedGraph()->SetMinimum(0);
+  eff->Draw("a2");
+  eff->Draw("same");
+  eff->Write();
+  c->Write();
 }
