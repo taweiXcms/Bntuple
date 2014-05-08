b3695bd73fbc32b5fe29d13fcebf8341b8670b41
gian michele innocenti <ginnocen@cern.ch>
2014-05-09 00:16:46 +0200

    update

--- CompareTrigger.C
+++ CompareTrigger.C
@@ -26,72 +26,130 @@ void CompareTrigger(){
   TH1F*hNewTrigger_Bzero=(TH1F*)fileNewTrigger_Bzero->Get("hPtCor");
   TH1F*hNewTrigger_Bs=(TH1F*)fileNewTrigger_Bs->Get("hPtCor");
    
-    
-  double relerr_numerator_Bplus=0.;
-  double relerr_denumerator_Bplus=0.;
-  double ratio_Bplus=0.;
-  double errratio_Bplus=0.;
+  TH1F*hYieldOldTrigger_Bplus=(TH1F*)fileOldTrigger_Bplus->Get("hPt");
+  TH1F*hYieldOldTrigger_Bzero=(TH1F*)fileOldTrigger_Bzero->Get("hPt");
+  TH1F*hYieldOldTrigger_Bs=(TH1F*)fileOldTrigger_Bs->Get("hPt");
   
+  TH1F*hYieldNewTrigger_Bplus=(TH1F*)fileNewTrigger_Bplus->Get("hPt");
+  TH1F*hYieldNewTrigger_Bzero=(TH1F*)fileNewTrigger_Bzero->Get("hPt");
+  TH1F*hYieldNewTrigger_Bs=(TH1F*)fileNewTrigger_Bs->Get("hPt");
+
+  double corryieldnew_Bplus=0.;
+  double corryieldold_Bplus=0.;
+  double ratiocorryield_Bplus=0.;
+  double errratiocorryield_Bplus=0.;
+    
+  double yieldnew_Bplus=0.;
+  double yieldold_Bplus=0.;
+  double ratioyield_Bplus=0.;
+  double errratioyield_Bplus=0.;
+  double relerrratioyield_Bplus=0.;
+    
   const int nBins_Bplus = 5;
-  double ptBins_Bplus[nBins_Bplus+1] = {10,15,20,25,30,60};
+  double ptBins_Bplus[nBins_Bplus+1] = {10.,15.,20.,25.,30.,60.};
+  double ptBinswidth[nBins_Bplus] = {5.,5.,5.,5.,30.};
+  
   TH1D *hRatioDoubleOverSingle_Bplus = new TH1D("hRatioDoubleOverSingle_Bplus","",nBins_Bplus,ptBins_Bplus);
   
   for (int i=1;i<=5;i++){
-  
-    relerr_numerator_Bplus=hNewTrigger_Bplus->GetBinError(i)/hNewTrigger_Bplus->GetBinContent(i);
-    relerr_denumerator_Bplus=hOldTrigger_Bplus->GetBinError(i)/hOldTrigger_Bplus->GetBinContent(i);
 
-    ratio_Bplus=hNewTrigger_Bplus->GetBinContent(i)/hOldTrigger_Bplus->GetBinContent(i);
-    errratio_Bplus=TMath::Sqrt(relerr_numerator_Bplus*relerr_numerator_Bplus+relerr_denumerator_Bplus*relerr_denumerator_Bplus)*ratio_Bplus;
-    hRatioDoubleOverSingle_Bplus->SetBinContent(i,ratio_Bplus);
-    hRatioDoubleOverSingle_Bplus->SetBinError(i,errratio_Bplus);
-
+    corryieldnew_Bplus=hNewTrigger_Bplus->GetBinContent(i);
+    corryieldold_Bplus=hOldTrigger_Bplus->GetBinContent(i);
+    yieldnew_Bplus=hYieldNewTrigger_Bplus->GetBinContent(i);
+    yieldold_Bplus=hYieldOldTrigger_Bplus->GetBinContent(i);
+    ratiocorryield_Bplus=corryieldnew_Bplus/corryieldold_Bplus;
+    ratioyield_Bplus=yieldnew_Bplus/yieldold_Bplus;
+    
+    //cout<<"yieldnew_Bplus"<<yieldnew_Bplus*ptBinswidth[i-1]<<"yieldold_Bplus"<<yieldold_Bplus*ptBinswidth[i-1]<<endl;
+    
+    hRatioDoubleOverSingle_Bplus->SetBinContent(i,ratiocorryield_Bplus);
+    errratioyield_Bplus=DoubleRatioError(yieldnew_Bplus*ptBinswidth[i-1],yieldold_Bplus*ptBinswidth[i-1]);
+    if(i==4) errratioyield_Bplus=ratioyield_Bplus*0.05;
+    relerrratioyield_Bplus=errratioyield_Bplus/ratioyield_Bplus;
+    errratiocorryield_Bplus=relerrratioyield_Bplus*ratiocorryield_Bplus;
+    hRatioDoubleOverSingle_Bplus->SetBinContent(i,ratiocorryield_Bplus);
+    hRatioDoubleOverSingle_Bplus->SetBinError(i,errratiocorryield_Bplus);
+    
+      
   }
-  
-  double relerr_numerator_Bzero=0.;
-  double relerr_denumerator_Bzero=0.;
-  double ratio_Bzero=0.;
-  double errratio_Bzero=0.;
-  
+
+  double corryieldnew_Bzero=0.;
+  double corryieldold_Bzero=0.;
+  double ratiocorryield_Bzero=0.;
+  double errratiocorryield_Bzero=0.;
+    
+  double yieldnew_Bzero=0.;
+  double yieldold_Bzero=0.;
+  double ratioyield_Bzero=0.;
+  double errratioyield_Bzero=0.;
+  double relerrratioyield_Bzero=0.;
+    
   const int nBins_Bzero = 3;
-  double ptBins_Bzero[nBins_Bzero+1] = {10,15,20,60};
+  double ptBins_Bzero[nBins_Bzero+1] = {10.,15.,20.,60.};
+  double ptBinswidth[nBins_Bzero] = {5.,5.,40.};
+  
   TH1D *hRatioDoubleOverSingle_Bzero = new TH1D("hRatioDoubleOverSingle_Bzero","",nBins_Bzero,ptBins_Bzero);
   
-  for (int i=1;i<=5;i++){
-  
-    relerr_numerator_Bzero=hNewTrigger_Bzero->GetBinError(i)/hNewTrigger_Bzero->GetBinContent(i);
-    relerr_denumerator_Bzero=hOldTrigger_Bzero->GetBinError(i)/hOldTrigger_Bzero->GetBinContent(i);
+  for (int i=1;i<=3;i++){
 
-    ratio_Bzero=hNewTrigger_Bzero->GetBinContent(i)/hOldTrigger_Bzero->GetBinContent(i);
-    errratio_Bzero=TMath::Sqrt(relerr_numerator_Bzero*relerr_numerator_Bzero+relerr_denumerator_Bzero*relerr_denumerator_Bzero)*ratio_Bzero;
-    hRatioDoubleOverSingle_Bzero->SetBinContent(i,ratio_Bzero);
-    hRatioDoubleOverSingle_Bzero->SetBinError(i,errratio_Bzero);
-
+    corryieldnew_Bzero=hNewTrigger_Bzero->GetBinContent(i);
+    corryieldold_Bzero=hOldTrigger_Bzero->GetBinContent(i);
+    yieldnew_Bzero=hYieldNewTrigger_Bzero->GetBinContent(i);
+    yieldold_Bzero=hYieldOldTrigger_Bzero->GetBinContent(i);
+    ratiocorryield_Bzero=corryieldnew_Bzero/corryieldold_Bzero;
+    ratioyield_Bzero=yieldnew_Bzero/yieldold_Bzero;
+    
+    //cout<<"yieldnew_Bzero"<<yieldnew_Bzero*ptBinswidth[i-1]<<"yieldold_Bzero"<<yieldold_Bzero*ptBinswidth[i-1]<<endl;
+    
+    hRatioDoubleOverSingle_Bzero->SetBinContent(i,ratiocorryield_Bzero);
+    errratioyield_Bzero=DoubleRatioError(yieldnew_Bzero*ptBinswidth[i-1],yieldold_Bzero*ptBinswidth[i-1]);
+    relerrratioyield_Bzero=errratioyield_Bzero/ratioyield_Bzero;
+    errratiocorryield_Bzero=relerrratioyield_Bzero*ratiocorryield_Bzero;
+    hRatioDoubleOverSingle_Bzero->SetBinContent(i,ratiocorryield_Bzero);
+    hRatioDoubleOverSingle_Bzero->SetBinError(i,errratiocorryield_Bzero);
+    
+      
   }
   
-  double relerr_numerator_Bs=0.;
-  double relerr_denumerator_Bs=0.;
-  double ratio_Bs=0.;
-  double errratio_Bs=0.;
-
-  
+  double corryieldnew_Bs=0.;
+  double corryieldold_Bs=0.;
+  double ratiocorryield_Bs=0.;
+  double errratiocorryield_Bs=0.;
+    
+  double yieldnew_Bs=0.;
+  double yieldold_Bs=0.;
+  double ratioyield_Bs=0.;
+  double errratioyield_Bs=0.;
+  double relerrratioyield_Bs=0.;
+    
   const int nBins_Bs = 1;
-  double ptBins_Bs[nBins_Bs+1] = {10,60};
+  double ptBins_Bs[nBins_Bs+1] = {10.,60.};
+  double ptBinswidth[nBins_Bs] = {50.};
+  
   TH1D *hRatioDoubleOverSingle_Bs = new TH1D("hRatioDoubleOverSingle_Bs","",nBins_Bs,ptBins_Bs);
   
   for (int i=1;i<=5;i++){
-  
-    relerr_numerator_Bs=hNewTrigger_Bs->GetBinError(i)/hNewTrigger_Bs->GetBinContent(i);
-    relerr_denumerator_Bs=hOldTrigger_Bs->GetBinError(i)/hOldTrigger_Bs->GetBinContent(i);
 
-    ratio_Bs=hNewTrigger_Bs->GetBinContent(i)/hOldTrigger_Bs->GetBinContent(i);
-    errratio_Bs=TMath::Sqrt(relerr_numerator_Bs*relerr_numerator_Bs+relerr_denumerator_Bs*relerr_denumerator_Bs)*ratio_Bs;
-    hRatioDoubleOverSingle_Bs->SetBinContent(i,ratio_Bs);
-    hRatioDoubleOverSingle_Bs->SetBinError(i,errratio_Bs);
-
+    corryieldnew_Bs=hNewTrigger_Bs->GetBinContent(i);
+    corryieldold_Bs=hOldTrigger_Bs->GetBinContent(i);
+    yieldnew_Bs=hYieldNewTrigger_Bs->GetBinContent(i);
+    yieldold_Bs=hYieldOldTrigger_Bs->GetBinContent(i);
+    ratiocorryield_Bs=corryieldnew_Bs/corryieldold_Bs;
+    ratioyield_Bs=yieldnew_Bs/yieldold_Bs;
+    
+    //cout<<"yieldnew_Bs"<<yieldnew_Bs*ptBinswidth[i-1]<<"yieldold_Bs"<<yieldold_Bs*ptBinswidth[i-1]<<endl;
+    
+    hRatioDoubleOverSingle_Bs->SetBinContent(i,ratiocorryield_Bs);
+    errratioyield_Bs=DoubleRatioError(yieldnew_Bs*ptBinswidth[i-1],yieldold_Bs*ptBinswidth[i-1]);
+    relerrratioyield_Bs=errratioyield_Bs/ratioyield_Bs;
+    errratiocorryield_Bs=relerrratioyield_Bs*ratiocorryield_Bs;
+    hRatioDoubleOverSingle_Bs->SetBinContent(i,ratiocorryield_Bs);
+    hRatioDoubleOverSingle_Bs->SetBinError(i,errratiocorryield_Bs);
+    
+      
   }
 
-
+  
 
 
   TCanvas *canvas=new TCanvas("canvas","canvas",1500,500);
@@ -154,9 +212,8 @@ void CompareTrigger(){
   hRatioDoubleOverSingle_Bs->SetLineWidth(2);
   hRatioDoubleOverSingle_Bs->Draw();
 
-
   canvas->SaveAs("Plots/canvasComparisonTriggerCorr.eps");
-  
+
   
 }
 
