1217fe9be62f6d130bfc276defd05f7cfd821c59
ginnocen <ginnocen@cern.ch>
2014-05-11 20:09:40 +0200

    update

---
+++ StudyTrigger/Display/CompareTrigger.C
@@ -0,0 +1,310 @@
+void CompareTrigger(){
+
+  double DoubleRatioError(double,double);
+
+
+  gROOT->SetStyle("Plain");	
+  gStyle->SetOptStat(0);
+  gStyle->SetOptStat(0000);
+  gStyle->SetPalette(0);
+  gStyle->SetCanvasColor(0);
+  gStyle->SetFrameFillColor(0);
+  gStyle->SetOptTitle(0);
+
+  TFile*fileOldTrigger_Bplus=new TFile("../InputSingleDouble/SigmaBplusSingle.root");
+  TFile*fileNewTrigger_Bplus=new TFile("../InputSingleDouble/SigmaBplusDouble.root");
+  TFile*fileOldTrigger_Bzero=new TFile("../InputSingleDouble/SigmaBzeroSingle.root");
+  TFile*fileNewTrigger_Bzero=new TFile("../InputSingleDouble/SigmaBzeroDouble.root");
+  TFile*fileOldTrigger_Bs=new TFile("../InputSingleDouble/SigmaBsSingle.root");
+  TFile*fileNewTrigger_Bs=new TFile("../InputSingleDouble/SigmaBsDouble.root");
+ 
+  TH1F*hOldTrigger_Bplus=(TH1F*)fileOldTrigger_Bplus->Get("hPtCor");
+  TH1F*hOldTrigger_Bzero=(TH1F*)fileOldTrigger_Bzero->Get("hPtCor");
+  TH1F*hOldTrigger_Bs=(TH1F*)fileOldTrigger_Bs->Get("hPtCor");
+  
+  TH1F*hNewTrigger_Bplus=(TH1F*)fileNewTrigger_Bplus->Get("hPtCor");
+  TH1F*hNewTrigger_Bzero=(TH1F*)fileNewTrigger_Bzero->Get("hPtCor");
+  TH1F*hNewTrigger_Bs=(TH1F*)fileNewTrigger_Bs->Get("hPtCor");
+   
+  TH1F*hYieldOldTrigger_Bplus=(TH1F*)fileOldTrigger_Bplus->Get("hPt");
+  TH1F*hYieldOldTrigger_Bzero=(TH1F*)fileOldTrigger_Bzero->Get("hPt");
+  TH1F*hYieldOldTrigger_Bs=(TH1F*)fileOldTrigger_Bs->Get("hPt");
+  
+  TH1F*hYieldNewTrigger_Bplus=(TH1F*)fileNewTrigger_Bplus->Get("hPt");
+  TH1F*hYieldNewTrigger_Bzero=(TH1F*)fileNewTrigger_Bzero->Get("hPt");
+  TH1F*hYieldNewTrigger_Bs=(TH1F*)fileNewTrigger_Bs->Get("hPt");
+
+  double corryieldnew_Bplus=0.;
+  double corryieldold_Bplus=0.;
+  double ratiocorryield_Bplus=0.;
+  double errratiocorryield_Bplus=0.;
+    
+  double yieldnew_Bplus=0.;
+  double yieldold_Bplus=0.;
+  double ratioyield_Bplus=0.;
+  double errratioyield_Bplus=0.;
+  double relerrratioyield_Bplus=0.;
+    
+  const int nBins_Bplus = 5;
+  double ptBins_Bplus[nBins_Bplus+1] = {10.,15.,20.,25.,30.,60.};
+  double ptBinswidth[nBins_Bplus] = {5.,5.,5.,5.,30.};
+  
+  TH1D *hRatioDoubleOverSingle_Bplus = new TH1D("hRatioDoubleOverSingle_Bplus","",nBins_Bplus,ptBins_Bplus);
+  TH1D *hRatioYieldDoubleOverSingle_Bplus = new TH1D("hRatioYieldDoubleOverSingle_Bplus","",nBins_Bplus,ptBins_Bplus);
+  
+  for (int i=1;i<=5;i++){
+
+    corryieldnew_Bplus=hNewTrigger_Bplus->GetBinContent(i);
+    corryieldold_Bplus=hOldTrigger_Bplus->GetBinContent(i);
+    yieldnew_Bplus=hYieldNewTrigger_Bplus->GetBinContent(i);
+    yieldold_Bplus=hYieldOldTrigger_Bplus->GetBinContent(i);
+    ratiocorryield_Bplus=corryieldnew_Bplus/corryieldold_Bplus;
+    ratioyield_Bplus=yieldnew_Bplus/yieldold_Bplus;
+    
+    //cout<<"yieldnew_Bplus"<<yieldnew_Bplus*ptBinswidth[i-1]<<"yieldold_Bplus"<<yieldold_Bplus*ptBinswidth[i-1]<<endl;
+    
+    hRatioDoubleOverSingle_Bplus->SetBinContent(i,ratiocorryield_Bplus);
+    errratioyield_Bplus=DoubleRatioError(yieldnew_Bplus*ptBinswidth[i-1],yieldold_Bplus*ptBinswidth[i-1]);
+    if(i==4) errratioyield_Bplus=ratioyield_Bplus*0.05;
+    relerrratioyield_Bplus=errratioyield_Bplus/ratioyield_Bplus;
+    errratiocorryield_Bplus=relerrratioyield_Bplus*ratiocorryield_Bplus;
+    hRatioDoubleOverSingle_Bplus->SetBinContent(i,ratiocorryield_Bplus);
+    hRatioDoubleOverSingle_Bplus->SetBinError(i,errratiocorryield_Bplus);
+    
+    hRatioYieldDoubleOverSingle_Bplus->SetBinContent(i,ratioyield_Bplus);
+    hRatioYieldDoubleOverSingle_Bplus->SetBinError(i,errratioyield_Bplus);
+      
+  }
+
+  double corryieldnew_Bzero=0.;
+  double corryieldold_Bzero=0.;
+  double ratiocorryield_Bzero=0.;
+  double errratiocorryield_Bzero=0.;
+    
+  double yieldnew_Bzero=0.;
+  double yieldold_Bzero=0.;
+  double ratioyield_Bzero=0.;
+  double errratioyield_Bzero=0.;
+  double relerrratioyield_Bzero=0.;
+    
+  const int nBins_Bzero = 3;
+  double ptBins_Bzero[nBins_Bzero+1] = {10.,15.,20.,60.};
+  double ptBinswidth[nBins_Bzero] = {5.,5.,40.};
+  
+  TH1D *hRatioDoubleOverSingle_Bzero = new TH1D("hRatioDoubleOverSingle_Bzero","",nBins_Bzero,ptBins_Bzero);
+  TH1D *hRatioYieldDoubleOverSingle_Bzero = new TH1D("hRatioYieldDoubleOverSingle_zero","",nBins_Bzero,ptBins_Bzero);
+
+    
+  
+  for (int i=1;i<=3;i++){
+
+    corryieldnew_Bzero=hNewTrigger_Bzero->GetBinContent(i);
+    corryieldold_Bzero=hOldTrigger_Bzero->GetBinContent(i);
+    yieldnew_Bzero=hYieldNewTrigger_Bzero->GetBinContent(i);
+    yieldold_Bzero=hYieldOldTrigger_Bzero->GetBinContent(i);
+    ratiocorryield_Bzero=corryieldnew_Bzero/corryieldold_Bzero;
+    ratioyield_Bzero=yieldnew_Bzero/yieldold_Bzero;
+    
+    //cout<<"yieldnew_Bzero"<<yieldnew_Bzero*ptBinswidth[i-1]<<"yieldold_Bzero"<<yieldold_Bzero*ptBinswidth[i-1]<<endl;
+    
+    hRatioDoubleOverSingle_Bzero->SetBinContent(i,ratiocorryield_Bzero);
+    errratioyield_Bzero=DoubleRatioError(yieldnew_Bzero*ptBinswidth[i-1],yieldold_Bzero*ptBinswidth[i-1]);
+    relerrratioyield_Bzero=errratioyield_Bzero/ratioyield_Bzero;
+    errratiocorryield_Bzero=relerrratioyield_Bzero*ratiocorryield_Bzero;
+    hRatioDoubleOverSingle_Bzero->SetBinContent(i,ratiocorryield_Bzero);
+    hRatioDoubleOverSingle_Bzero->SetBinError(i,errratiocorryield_Bzero);
+    hRatioYieldDoubleOverSingle_Bzero->SetBinContent(i,ratioyield_Bzero);
+    hRatioYieldDoubleOverSingle_Bzero->SetBinError(i,errratioyield_Bzero);
+
+    
+      
+  }
+  
+  double corryieldnew_Bs=0.;
+  double corryieldold_Bs=0.;
+  double ratiocorryield_Bs=0.;
+  double errratiocorryield_Bs=0.;
+    
+  double yieldnew_Bs=0.;
+  double yieldold_Bs=0.;
+  double ratioyield_Bs=0.;
+  double errratioyield_Bs=0.;
+  double relerrratioyield_Bs=0.;
+    
+  const int nBins_Bs = 1;
+  double ptBins_Bs[nBins_Bs+1] = {10.,60.};
+  double ptBinswidth[nBins_Bs] = {50.};
+  TH1D *hRatioDoubleOverSingle_Bs = new TH1D("hRatioDoubleOverSingle_Bs","",nBins_Bs,ptBins_Bs);
+  TH1D *hRatioYieldDoubleOverSingle_Bs = new TH1D("hRatioYieldDoubleOverSingle_Bs","",nBins_Bs,ptBins_Bs);
+
+    
+  for (int i=1;i<=1;i++){
+
+    corryieldnew_Bs=hNewTrigger_Bs->GetBinContent(i);
+    corryieldold_Bs=hOldTrigger_Bs->GetBinContent(i);
+    yieldnew_Bs=hYieldNewTrigger_Bs->GetBinContent(i);
+    yieldold_Bs=hYieldOldTrigger_Bs->GetBinContent(i);
+    ratiocorryield_Bs=corryieldnew_Bs/corryieldold_Bs;
+    ratioyield_Bs=yieldnew_Bs/yieldold_Bs;
+    
+    //cout<<"yieldnew_Bs"<<yieldnew_Bs*ptBinswidth[i-1]<<"yieldold_Bs"<<yieldold_Bs*ptBinswidth[i-1]<<endl;
+    
+    hRatioDoubleOverSingle_Bs->SetBinContent(i,ratiocorryield_Bs);
+    errratioyield_Bs=DoubleRatioError(yieldnew_Bs*ptBinswidth[i-1],yieldold_Bs*ptBinswidth[i-1]);
+    relerrratioyield_Bs=errratioyield_Bs/ratioyield_Bs;
+    errratiocorryield_Bs=relerrratioyield_Bs*ratiocorryield_Bs;
+    hRatioDoubleOverSingle_Bs->SetBinContent(i,ratiocorryield_Bs);
+    hRatioDoubleOverSingle_Bs->SetBinError(i,errratiocorryield_Bs);
+    hRatioYieldDoubleOverSingle_Bs->SetBinContent(i,ratioyield_Bzero);
+    hRatioYieldDoubleOverSingle_Bs->SetBinError(i,errratioyield_Bzero);
+
+    
+      
+  }
+
+  
+
+
+  TCanvas *canvasCorrected=new TCanvas("canvasCorrected","canvasCorrected",1200,400);
+  canvasCorrected->Divide(3,1);
+  canvasCorrected->cd(1);  
+  hRatioDoubleOverSingle_Bplus->GetXaxis()->SetTitle("B^{+} p_{T}");
+  hRatioDoubleOverSingle_Bplus->GetYaxis()->SetTitle("DoubleMuOpen / SingleMu3 B^{+}");
+  hRatioDoubleOverSingle_Bplus->GetXaxis()->SetTitleOffset(.88);
+  hRatioDoubleOverSingle_Bplus->GetYaxis()->SetTitleOffset(1.04);
+  hRatioDoubleOverSingle_Bplus->GetXaxis()->SetTitleSize(0.045);
+  hRatioDoubleOverSingle_Bplus->GetYaxis()->SetTitleSize(0.045);
+  hRatioDoubleOverSingle_Bplus->GetXaxis()->SetTitleFont(42);
+  hRatioDoubleOverSingle_Bplus->GetYaxis()->SetTitleFont(42);
+  hRatioDoubleOverSingle_Bplus->GetXaxis()->SetLabelFont(42);
+  hRatioDoubleOverSingle_Bplus->GetYaxis()->SetLabelFont(42);
+  hRatioDoubleOverSingle_Bplus->GetXaxis()->SetLabelSize(0.037);
+  hRatioDoubleOverSingle_Bplus->GetYaxis()->SetLabelSize(0.037);  
+  hRatioDoubleOverSingle_Bplus->SetMinimum(0.);
+  hRatioDoubleOverSingle_Bplus->SetMaximum(2.);
+  hRatioDoubleOverSingle_Bplus->SetMarkerSize(1.2);
+  hRatioDoubleOverSingle_Bplus->SetLineColor(1);
+  hRatioDoubleOverSingle_Bplus->SetLineWidth(2);
+  hRatioDoubleOverSingle_Bplus->SetMarkerStyle(21);
+  hRatioDoubleOverSingle_Bplus->SetMarkerSize(1);
+  hRatioDoubleOverSingle_Bplus->Draw("pe");
+  hRatioYieldDoubleOverSingle_Bplus->SetMarkerSize(1.2);
+  hRatioYieldDoubleOverSingle_Bplus->SetMinimum(0.);
+  hRatioYieldDoubleOverSingle_Bplus->SetMaximum(2.);
+  hRatioYieldDoubleOverSingle_Bplus->SetLineColor(2);
+  hRatioYieldDoubleOverSingle_Bplus->SetLineWidth(2);  
+  hRatioYieldDoubleOverSingle_Bplus->SetMarkerStyle(20);
+  hRatioYieldDoubleOverSingle_Bplus->SetMarkerColor(2);
+  hRatioYieldDoubleOverSingle_Bplus->SetMarkerSize(1);
+  hRatioYieldDoubleOverSingle_Bplus->Draw("pesame");
+  
+  TLegend *leg_Bplus = new TLegend(0.297315,0.6173362,0.8590604,0.7463002,NULL,"brNDC");
+  leg_Bplus->AddEntry(hRatioYieldDoubleOverSingle_Bplus,"Yields uncorrected","pl");
+  leg_Bplus->AddEntry(hRatioDoubleOverSingle_Bplus,"dN/dp_{T} fully corrected","pl");
+  leg_Bplus->SetBorderSize(0);
+  leg_Bplus->SetFillStyle(0);
+  leg_Bplus->SetTextSize(0.05);
+  leg_Bplus->SetTextFont(42);
+  leg_Bplus->Draw();
+
+
+
+  canvasCorrected->cd(2);  
+  hRatioDoubleOverSingle_Bzero->GetXaxis()->SetTitle("B^{0} p_{T}");
+  hRatioDoubleOverSingle_Bzero->GetYaxis()->SetTitle("DoubleMuOpen / SingleMu3 B^{0}");
+  hRatioDoubleOverSingle_Bzero->GetXaxis()->SetTitleOffset(.88);
+  hRatioDoubleOverSingle_Bzero->GetYaxis()->SetTitleOffset(1.04);
+  hRatioDoubleOverSingle_Bzero->GetXaxis()->SetTitleSize(0.045);
+  hRatioDoubleOverSingle_Bzero->GetYaxis()->SetTitleSize(0.045);
+  hRatioDoubleOverSingle_Bzero->GetXaxis()->SetTitleFont(42);
+  hRatioDoubleOverSingle_Bzero->GetYaxis()->SetTitleFont(42);
+  hRatioDoubleOverSingle_Bzero->GetXaxis()->SetLabelFont(42);
+  hRatioDoubleOverSingle_Bzero->GetYaxis()->SetLabelFont(42);
+  hRatioDoubleOverSingle_Bzero->GetXaxis()->SetLabelSize(0.037);
+  hRatioDoubleOverSingle_Bzero->GetYaxis()->SetLabelSize(0.037);  
+  hRatioDoubleOverSingle_Bzero->SetMinimum(0.);
+  hRatioDoubleOverSingle_Bzero->SetMaximum(2.);
+  hRatioDoubleOverSingle_Bzero->SetMarkerSize(1.2);
+  hRatioDoubleOverSingle_Bzero->SetLineColor(1);
+  hRatioDoubleOverSingle_Bzero->SetLineWidth(2);
+  hRatioDoubleOverSingle_Bzero->SetMarkerStyle(21);
+  hRatioDoubleOverSingle_Bzero->SetMarkerSize(1);
+  hRatioDoubleOverSingle_Bzero->Draw("pe");
+  hRatioYieldDoubleOverSingle_Bzero->SetMarkerSize(1.2);
+  hRatioYieldDoubleOverSingle_Bzero->SetMinimum(0.);
+  hRatioYieldDoubleOverSingle_Bzero->SetMaximum(2.);
+  hRatioYieldDoubleOverSingle_Bzero->SetLineColor(2);
+  hRatioYieldDoubleOverSingle_Bzero->SetLineWidth(2);  
+  hRatioYieldDoubleOverSingle_Bzero->SetMarkerStyle(20);
+  hRatioYieldDoubleOverSingle_Bzero->SetMarkerColor(2);
+  hRatioYieldDoubleOverSingle_Bzero->SetMarkerSize(1);
+  hRatioYieldDoubleOverSingle_Bzero->Draw("pesame");
+  
+  TLegend *leg_Bzero = new TLegend(0.297315,0.6173362,0.8590604,0.7463002,NULL,"brNDC");
+  leg_Bzero->AddEntry(hRatioYieldDoubleOverSingle_Bzero,"Yields uncorrected","pl");
+  leg_Bzero->AddEntry(hRatioDoubleOverSingle_Bzero,"dN/dp_{T} fully corrected","pl");
+  leg_Bzero->SetBorderSize(0);
+  leg_Bzero->SetFillStyle(0);
+  leg_Bzero->SetTextSize(0.05);
+  leg_Bzero->SetTextFont(42);
+  leg_Bzero->Draw();
+
+
+  
+
+  canvasCorrected->cd(3);  
+  hRatioDoubleOverSingle_Bs->GetXaxis()->SetTitle("B_{s} p_{T}");
+  hRatioDoubleOverSingle_Bs->GetYaxis()->SetTitle("DoubleMuOpen / SingleMu3 B_{s}");
+  hRatioDoubleOverSingle_Bs->GetXaxis()->SetTitleOffset(.88);
+  hRatioDoubleOverSingle_Bs->GetYaxis()->SetTitleOffset(1.04);
+  hRatioDoubleOverSingle_Bs->GetXaxis()->SetTitleSize(0.045);
+  hRatioDoubleOverSingle_Bs->GetYaxis()->SetTitleSize(0.045);
+  hRatioDoubleOverSingle_Bs->GetXaxis()->SetTitleFont(42);
+  hRatioDoubleOverSingle_Bs->GetYaxis()->SetTitleFont(42);
+  hRatioDoubleOverSingle_Bs->GetXaxis()->SetLabelFont(42);
+  hRatioDoubleOverSingle_Bs->GetYaxis()->SetLabelFont(42);
+  hRatioDoubleOverSingle_Bs->GetXaxis()->SetLabelSize(0.037);
+  hRatioDoubleOverSingle_Bs->GetYaxis()->SetLabelSize(0.037);  
+  hRatioDoubleOverSingle_Bs->SetMinimum(0.);
+  hRatioDoubleOverSingle_Bs->SetMaximum(2.);
+  hRatioDoubleOverSingle_Bs->SetMarkerSize(1.2);
+  hRatioDoubleOverSingle_Bs->SetLineColor(1);
+  hRatioDoubleOverSingle_Bs->SetLineWidth(2);
+  hRatioDoubleOverSingle_Bs->SetMarkerStyle(21);
+  hRatioDoubleOverSingle_Bs->SetMarkerSize(1);
+  hRatioDoubleOverSingle_Bs->Draw("pe");
+  hRatioYieldDoubleOverSingle_Bs->SetMarkerSize(1.2);
+  hRatioYieldDoubleOverSingle_Bs->SetMinimum(0.);
+  hRatioYieldDoubleOverSingle_Bs->SetMaximum(2.);
+  hRatioYieldDoubleOverSingle_Bs->SetLineColor(2);
+  hRatioYieldDoubleOverSingle_Bs->SetLineWidth(2);  
+  hRatioYieldDoubleOverSingle_Bs->SetMarkerStyle(20);
+  hRatioYieldDoubleOverSingle_Bs->SetMarkerColor(2);
+  hRatioYieldDoubleOverSingle_Bs->SetMarkerSize(1);
+  hRatioYieldDoubleOverSingle_Bs->Draw("pesame");
+  
+  TLegend *leg_Bs = new TLegend(0.297315,0.6173362,0.8590604,0.7463002,NULL,"brNDC");
+  leg_Bs->AddEntry(hRatioYieldDoubleOverSingle_Bs,"Yields uncorrected","pl");
+  leg_Bs->AddEntry(hRatioDoubleOverSingle_Bs,"dN/dp_{T} fully corrected","pl");
+  leg_Bs->SetBorderSize(0);
+  leg_Bs->SetFillStyle(0);
+  leg_Bs->SetTextSize(0.05);
+  leg_Bs->SetTextFont(42);
+  leg_Bs->Draw();
+
+
+
+  canvasCorrected->SaveAs("Plots/canvasComparisonTriggerCorr.eps");
+}
+
+
+double DoubleRatioError(double Num=-1,double Den=-1){
+
+  double ratio,errratio;
+
+  ratio=Num/Den;
+  errratio=TMath::Sqrt(ratio*(1-ratio)/Den);
+  return errratio;
+}
+
+
---
+++ StudyTrigger/Display/StudyTriggerOverlap.C
@@ -0,0 +1,194 @@
+#include <iostream>                                                                                                                                                                                         
+#include <utility>
+#include <TRandom.h>
+#include <TRandom3.h>
+#include <TH1D.h>
+#include <TH1F.h>
+#include <TCut.h>
+#include <TH2F.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TMath.h>
+#include <TLegend.h>
+#include <TLatex.h>
+#include <TNtuple.h>
+#include <TLorentzVector.h>
+#define MAX_XB 8192
+#define MAX_GEN 4096 
+#define MAX_GEN 4096 //default 2048
+#define MUON_MASS   0.10565837
+#define PION_MASS   0.13957018
+#define KAON_MASS   0.493677
+#define KSHORT_MASS 0.497614
+#define KSTAR_MASS  0.89594
+#define PHI_MASS    1.019455
+#define JPSI_MASS   3.096916
+#define PSI2S_MASS  3.686109
+
+//TString inputmc="/data/bmeson/MC/nt_BoostedMC_20140510_Kp_TriggerMatchingMuon.root";
+TString inputdata="/data/bmeson/data/nt_20140510_PAMuon_HIRun2013_PromptrecoAndRereco_v1_MuonMatching_EvtBase_skim.root";
+
+
+//const int nBins=5;
+//double ptBins[nBins+1]={10,15,20,25,30,60};
+
+int NCandidate_All=0;
+int NCandidate_SignRegion=0;
+int NCandidate_SideBand=0;
+
+int NCandidateSingle_All=0;
+int NCandidateDouble_All=0;
+int NCandidateSingleAndDouble_All=0;
+int NCandidateSingleAndNotDouble_All=0;
+int NCandidateNotSingleAndDouble_All=0;
+
+int NCandidateSingle_SignRegion=0;
+int NCandidateDouble_SignRegion=0;
+int NCandidateSingleAndDouble_SignRegion=0;
+int NCandidateSingleAndNotDouble_SignRegion=0;
+int NCandidateNotSingleAndDouble_SignRegion=0;
+
+int NCandidateSingle_SideBand=0;
+int NCandidateDouble_SideBand=0;
+int NCandidateSingleAndDouble_SideBand=0;
+int NCandidateSingleAndNotDouble_SideBand=0;
+int NCandidateNotSingleAndDouble_SideBand=0;
+
+void StudyTriggerOverlap(){
+
+  TFile *inf = new TFile(inputdata.Data());
+  TTree *nt = (TTree*) inf->Get("ntKp");
+
+  int size;                                                                                                                                                                                               
+  float y[MAX_GEN];
+  float eta[MAX_GEN];
+  float pt[MAX_GEN];
+  float phi[MAX_GEN];
+  float pdgId[MAX_GEN];
+  float isSignal[MAX_GEN];
+  float mu1pt[MAX_GEN];
+  float mu2pt[MAX_GEN];
+  float mu1eta[MAX_GEN];
+  float mu2eta[MAX_GEN];
+  
+  int   HLT_PAMu3_v1;
+  int HLT_PAL1DoubleMuOpen_v1;
+  float y[MAX_XB];
+  float pt[MAX_XB];
+  float mass[MAX_XB];
+  int isbestchi2[MAX_XB];
+  float trk1Pt[MAX_XB];
+  float trk2Pt[MAX_XB];
+  float chi2cl[MAX_XB];
+  float d0[MAX_XB];
+  float d0Err[MAX_XB];
+  float dtheta[MAX_XB];
+  float tktkmass[MAX_XB];
+  float mumumass[MAX_XB];
+  float gen[MAX_XB];
+  float mu1pt[MAX_XB];
+  float mu2pt[MAX_XB];
+  float mu1eta[MAX_XB];
+  float mu2eta[MAX_XB];
+
+  nt->SetBranchAddress("size",&size);
+  nt->SetBranchAddress("y",&y);
+  nt->SetBranchAddress("eta",eta);
+  nt->SetBranchAddress("phi",phi);
+  nt->SetBranchAddress("pt",pt);
+  nt->SetBranchAddress("pdgId",pdgId);
+  nt->SetBranchAddress("isSignal",isSignal);
+  nt->SetBranchAddress("mu1pt",mu1pt);
+  nt->SetBranchAddress("mu2pt",mu2pt);
+  nt->SetBranchAddress("mu1eta",mu1eta);
+  nt->SetBranchAddress("mu2eta",mu2eta);
+  nt->SetBranchAddress("size",&size);
+  nt->SetBranchAddress("HLT_PAMu3_v1",&HLT_PAMu3_v1);
+  nt->SetBranchAddress("HLT_PAL1DoubleMuOpen_v1",&HLT_PAL1DoubleMuOpen_v1);
+  nt->SetBranchAddress("pt",pt);
+  nt->SetBranchAddress("y",y);
+  nt->SetBranchAddress("mass",mass);
+  nt->SetBranchAddress("isbestchi2",isbestchi2);
+  nt->SetBranchAddress("trk1Pt",trk1Pt);
+  nt->SetBranchAddress("trk2Pt",trk2Pt);
+  nt->SetBranchAddress("chi2cl",chi2cl);
+  nt->SetBranchAddress("d0",d0);
+  nt->SetBranchAddress("d0Err",d0Err);
+  nt->SetBranchAddress("dtheta",dtheta);
+  nt->SetBranchAddress("tktkmass",tktkmass);
+  nt->SetBranchAddress("mumumass",mumumass);
+  nt->SetBranchAddress("gen",gen);
+  nt->SetBranchAddress("mu1pt",mu1pt);
+  nt->SetBranchAddress("mu2pt",mu2pt);
+  nt->SetBranchAddress("mu1eta",mu1eta);
+  nt->SetBranchAddress("mu2eta",mu2eta);
+  
+  int nevents_total = nt->GetEntries();    
+  //nevents_total=1000000; 
+  for(int entry=0; entry<nevents_total; entry++){
+    if((entry%10000)==0) printf("Loading event #%d of %d.\n",entry,nevents_total);
+    nt->GetEntry(entry);
+
+    for(int c=0; c<size; c++){
+                 
+      if(pt[c]>10. && pt[c]<15.){
+          
+        if((abs(mumumass[c]-3.096916)<0.15&&mass[c]>5&&mass[c]<6&& isbestchi2[c]&&trk1Pt[c]>0.9&& chi2cl[c]>1.32e-02&&(d0[c]/d0Err[c])>3.41&&cos(dtheta[c])>-3.46e-01)){
+          NCandidate_All++;
+          
+          if(HLT_PAMu3_v1[c]) NCandidateSingle_All++;
+          if(HLT_PAL1DoubleMuOpen_v1[c]) NCandidateDouble_All++;
+          if(HLT_PAMu3_v1[c] && HLT_PAL1DoubleMuOpen_v1[c]) NCandidateSingleAndDouble_All++;
+          if(HLT_PAMu3_v1[c] && !HLT_PAL1DoubleMuOpen_v1[c]) NCandidateSingleAndNotDouble_All++;
+          if(!HLT_PAMu3_v1[c] && HLT_PAL1DoubleMuOpen_v1[c]) NCandidateNotSingleAndDouble_All++;
+
+        }
+        
+        if((abs(mumumass[c]-3.096916)<0.15&&mass[c]>5&&mass[c]<6&& isbestchi2[c]&&trk1Pt[c]>0.9&& chi2cl[c]>1.32e-02&&(d0[c]/d0Err[c])>3.41&&cos(dtheta[c])>-3.46e-01) && (mass[c]>5.2 && mass[c]<5.4)){
+          NCandidate_SignRegion++;
+          
+          if(HLT_PAMu3_v1[c]) NCandidateSingle_SignRegion++;
+          if(HLT_PAL1DoubleMuOpen_v1[c]) NCandidateDouble_SignRegion++;
+          if(HLT_PAMu3_v1[c] && HLT_PAL1DoubleMuOpen_v1[c]) NCandidateSingleAndDouble_SignRegion++;
+          if(HLT_PAMu3_v1[c] && !HLT_PAL1DoubleMuOpen_v1[c]) NCandidateSingleAndNotDouble_SignRegion++;
+          if(!HLT_PAMu3_v1[c] && HLT_PAL1DoubleMuOpen_v1[c]) NCandidateNotSingleAndDouble_SignRegion++;
+
+        }
+        
+        if((abs(mumumass[c]-3.096916)<0.15&&mass[c]>5&&mass[c]<6&& isbestchi2[c]&&trk1Pt[c]>0.9&& chi2cl[c]>1.32e-02&&(d0[c]/d0Err[c])>3.41&&cos(dtheta[c])>-3.46e-01) && (mass[c]<5.2 || mass[c]>5.4)){
+          NCandidate_SideBand++;
+          
+          if(HLT_PAMu3_v1[c]) NCandidateSingle_SideBand++;
+          if(HLT_PAL1DoubleMuOpen_v1[c]) NCandidateDouble_SideBand++;
+          if(HLT_PAMu3_v1[c] && HLT_PAL1DoubleMuOpen_v1[c]) NCandidateSingleAndDouble_SideBand++;
+          if(HLT_PAMu3_v1[c] && !HLT_PAL1DoubleMuOpen_v1[c]) NCandidateSingleAndNotDouble_SideBand++;
+          if(!HLT_PAMu3_v1[c] && HLT_PAL1DoubleMuOpen_v1[c]) NCandidateNotSingleAndDouble_SideBand++;
+
+        }
+      }
+    }
+  }
+  cout<<"Invariant mass range 5<mass<6"<<endl;
+  cout<<"% of candidate selected by single ="<<(double)(NCandidateSingle_All)/(double)(NCandidate_All)<<endl;
+  cout<<"% of candidate selected by double ="<<(double)(NCandidateDouble_All)/(double)(NCandidate_All)<<endl;
+  cout<<"% of candidate selected by single and double ="<<(double)(NCandidateSingleAndDouble_All)/(double)(NCandidate_All)<<endl;
+  cout<<"% of candidate selected by single and not by double ="<<(double)(NCandidateSingleAndNotDouble_All)/(double)(NCandidate_All)<<endl;
+  cout<<"% of candidate selected by double and not by single ="<<(double)(NCandidateNotSingleAndDouble_All)/(double)(NCandidate_All)<<endl;
+  
+  cout<<"Invariant mass range 5.2<mass<5.4"<<endl;
+  cout<<"% of candidate selected by single ="<<(double)(NCandidateSingle_SignRegion)/(double)(NCandidate_SignRegion)<<endl;
+  cout<<"% of candidate selected by double ="<<(double)(NCandidateDouble_SignRegion)/(double)(NCandidate_SignRegion)<<endl;
+  cout<<"% of candidate selected by single and double ="<<(double)(NCandidateSingleAndDouble_SignRegion)/(double)(NCandidate_SignRegion)<<endl;
+  cout<<"% of candidate selected by single and not by double ="<<(double)(NCandidateSingleAndNotDouble_SignRegion)/(double)(NCandidate_SignRegion)<<endl;
+  cout<<"% of candidate selected by double and not by single ="<<(double)(NCandidateNotSingleAndDouble_SignRegion)/(double)(NCandidate_SignRegion)<<endl;
+
+  cout<<"Invariant mass range mass<5.2 or mass>5.4"<<endl;
+  cout<<"% of candidate selected by single ="<<(double)(NCandidateSingle_SideBand)/(double)(NCandidate_SideBand)<<endl;
+  cout<<"% of candidate selected by double ="<<(double)(NCandidateDouble_SideBand)/(double)(NCandidate_SideBand)<<endl;
+  cout<<"% of candidate selected by single and double ="<<(double)(NCandidateSingleAndDouble_SideBand)/(double)(NCandidate_SideBand)<<endl;
+  cout<<"% of candidate selected by single and not by double ="<<(double)(NCandidateSingleAndNotDouble_SideBand)/(double)(NCandidate_SideBand)<<endl;
+  cout<<"% of candidate selected by double and not by single ="<<(double)(NCandidateNotSingleAndDouble_SideBand)/(double)(NCandidate_SideBand)<<endl;
+
+}
