40b8089a4dea3d706d394357f9ac0358bec5ede7
gian michele innocenti <ginnocen@cern.ch>
2014-08-02 17:48:47 +0200

    acceptance vs y included

--- Acceptance/Code/Acceptance.C
+++ Acceptance/Code/Acceptance.C
@@ -5,43 +5,48 @@
 #include "TH1D.h"
 #define NUM_BX 9000
 
-const int nBins = 10;
+
+const int nBins = 5;
+double ptBins[nBins+1] = {10,15,20,25,30,60};
+  
+const int nBinsy = 5;
+double yBins[nBinsy+1] = {-2.4,-1.46,-0.46,0.46,1.46,2.4};
+
 double trkPtCut=0.9;
 double ptmax=60.;
 double ptmin=10.;
 
 void Acceptance(){
-
-  TH1D* LoopGen(TTree*,double,double);
-  TH1D* LoopAccNum(TTree*,double,double);
-  TH1D* LoopAccNum(TTree*,double,double);
   
-  TString infnamemc="/data/bmeson/MC/nt_MixMC_20140503_Kp__TriggerMatchingMuon_EvtBase_skim.root";
+  TString infnamemc="/Users/ginnocen/BAnalysis/InputMCexample/nt_MixMC_20140503_Kp__TriggerMatchingMuon_EvtBase_skim.root";
   TFile *filemc = new TFile(infnamemc.Data());
   TTree *nt_mcgen = (TTree*)filemc->Get("ntGen");
   TTree *nt = (TTree*)filemc->Get("ntKp");
   
-  TFile*fileinputreweight=new TFile("../InputFile/InputFilesMCData.root");
-
-  TH1D *hPtData = (TH1D*)fileinputreweight->Get("hPtData");
-  TH1D *hPtMC = (TH1D*)fileinputreweight->Get("hPtData");
-  TH1D *hyData = (TH1D*)fileinputreweight->Get("hPtData");
-  TH1D *hyMC = (TH1D*)fileinputreweight->Get("hPtData");
+  TFile*fileinputreweight=new TFile("../InputFile/FunctionsReweighting.root");
+  TF1*fPt=(TF1*)fileinputreweight->Get("fPt");
+  TF1*fy=(TF1*)fileinputreweight->Get("fy");
+      
+  TH1D *hPtAccNumVsPt = new TH1D("hPtAccNumVsPt","",nBins,ptBins);
+  TH1D *hPtAccDenVsPt = new TH1D("hPtAccDenVsPt","",nBins,ptBins);
+  TH1D *hPtAccNumVsPtReweight = new TH1D("hPtAccNumVsPtReweight","",nBins,ptBins);
+  TH1D *hPtAccDenVsPtReweight = new TH1D("hPtAccDenVsPtReweight","",nBins,ptBins);
   
-  TH1D*hReweightDataOverMC_Pt=(TH1D*)hPtData->Clone("hReweightDataOverMC_Pt");
-  TH1D*hReweightDataOverMC_y=(TH1D*)hyData->Clone("hReweightDataOverMC_y");
-  
-  hReweightDataOverMC_Pt->Divide(hPtMC);
-  hReweightDataOverMC_y->Divide(hyMC);
-  
-  TH1D *hPtAccNumVsPt = new TH1D("hPtAccNumVsPt","",nBins,10.,60.);
-  TH1D *hPtAccDenVsPt = new TH1D("hPtAccDenVsPt","",nBins,10.,60.);
+  TH1D *hPtAccNumVsy = new TH1D("hPtAccNumVsy","",nBinsy,yBins);
+  TH1D *hPtAccDenVsy = new TH1D("hPtAccDenVsy","",nBinsy,yBins);
+  TH1D *hPtAccNumVsyReweight = new TH1D("hPtAccNumVsyReweight","",nBinsy,yBins);
+  TH1D *hPtAccDenVsyReweight = new TH1D("hPtAccDenVsyReweight","",nBinsy,yBins);
   
   hPtAccNumVsPt->Sumw2();
   hPtAccDenVsPt->Sumw2();
+  hPtAccNumVsPtReweight->Sumw2();
+  hPtAccDenVsPtReweight->Sumw2();
   
-  
-  
+  hPtAccNumVsy->Sumw2();
+  hPtAccDenVsy->Sumw2();
+  hPtAccNumVsyReweight->Sumw2();
+  hPtAccDenVsyReweight->Sumw2();
+
   bool cut_yvsRun,cut_pt,cut_trk1Pt,cut_trk1eta,cut_mu1acc,cut_mu2acc;
   
   int Run,size;
@@ -74,43 +79,60 @@ void Acceptance(){
   nt->SetBranchAddress("Run",&Run);
 
   Int_t entries = (Int_t)nt_mcgen->GetEntries();
-  
+
   for (int i=0; i<entries; i++) {
     nt_mcgen->GetEntry(i);        
     nt->GetEntry(i);        
     for(int j=0;j<size;j++){
-      cut_yvsRun=((Run<=1&&abs(y[j]+0.465)<1.93)||(Run>1&&abs(y[j]-0.465)<1.93))&&abs(pdgId[j])==521&&isSignal[j]==1;
-      //cut_yvsRun=abs(y[j])<2.4&&abs(pdgId[j])==521&&isSignal[j]==1;
+    
+     cut_yvsRun=abs(y[j])<2.4&&abs(pdgId[j])==521&&isSignal[j]==1;
       cut_pt=((pt[j]>ptmin)&&(pt[j]<ptmax));
       if(cut_pt&&cut_yvsRun) {
+      
         hPtAccDenVsPt->Fill(pt[j]);  
+        hPtAccDenVsPtReweight->Fill(pt[j],fy->Eval(y[j])); 
+        hPtAccDenVsy->Fill(y[j]); 
+        hPtAccDenVsyReweight->Fill(y[j],fPt->Eval(pt[j]));
+
         cut_mu1acc=(abs(mu1eta[j])<1.3&&mu1pt[j]>3.3)||(abs(mu1eta[j])>1.3&&abs(mu1eta[j])<2.2&&mu1p[j]>2.9)||(abs(mu1eta[j])>2.2&&abs(mu1eta[j])<2.4&&mu1pt[j]>0.8);
 	    cut_mu2acc=(abs(mu2eta[j])<1.3&&mu2pt[j]>3.3)||(abs(mu2eta[j])>1.3&&abs(mu2eta[j])<2.2&&mu2p[j]>2.9)||(abs(mu2eta[j])>2.2&&abs(mu2eta[j])<2.4&&mu2pt[j]>0.8);
 	    cut_trk1Pt=(trk1Pt[j]>trkPtCut);
 	    cut_trk1eta=(abs(trk1eta[j])<2.4);
         if(cut_mu1acc&&cut_mu2acc&&cut_trk1Pt&&cut_trk1eta){
           hPtAccNumVsPt->Fill(pt[j]);  
+          hPtAccNumVsPtReweight->Fill(pt[j],fy->Eval(y[j])); 
+          hPtAccNumVsy->Fill(y[j]); 
+          hPtAccNumVsyReweight->Fill(y[j],fPt->Eval(pt[j]));
         }
-      }    	  
-  	}//loop over candidates
+      }  	  
+    }//loop over candidates
   }// loop over events
 
   
   TH1D *hAccVsPt = (TH1D*)hPtAccNumVsPt->Clone("hAccVsPt");  
   hAccVsPt->Sumw2();
   hAccVsPt->Divide(hAccVsPt, hPtAccDenVsPt, 1.0, 1.0, "B");
+  TH1D *hAccVsPtReweight = (TH1D*)hPtAccNumVsPtReweight->Clone("hAccVsPtReweight");  
+  hAccVsPtReweight->Sumw2();
+  hAccVsPtReweight->Divide(hAccVsPtReweight, hPtAccDenVsPtReweight, 1.0, 1.0, "B");
+
+  TH1D *hAccVsy = (TH1D*)hPtAccNumVsy->Clone("hAccVsy");  
+  hAccVsy->Sumw2();
+  hAccVsy->Divide(hAccVsy, hPtAccDenVsy, 1.0, 1.0, "B");
+  TH1D *hAccVsyReweight = (TH1D*)hPtAccNumVsyReweight->Clone("hAccVsyReweight");  
+  hAccVsyReweight->Sumw2();
+  hAccVsyReweight->Divide(hAccVsyReweight, hPtAccDenVsyReweight, 1.0, 1.0, "B");
+
 
   TFile*fileoutput=new TFile("Results/outputBplus.root","recreate");
   fileoutput->cd();
   hAccVsPt->Write();
+  hAccVsPtReweight->Write();
   hPtAccNumVsPt->Write();
-  hPtAccDenVsPt->Write();  
-  
-  TCanvas*canvasreweight=new TCanvas("canvasreweight","canvasreweight",1000,500);
-  canvasreweight->Divide(2,1);
-  canvasreweight->cd(1);
-  hReweightDataOverMC_Pt->Draw();
-  canvasreweight->cd(2);
-  hReweightDataOverMC_y->Draw();
-  
+  hPtAccDenVsPt->Write(); 
+  hAccVsy->Write();
+  hAccVsyReweight->Write();
+  hPtAccNumVsy->Write();
+  hPtAccDenVsy->Write();  
+ 
 }
--- Acceptance/InputFile/FakeInput.C
+++ Acceptance/InputFile/FakeInput.C
@@ -1,10 +1,11 @@
-void FakeInput(){
-
   const int nBins = 5;
   double ptBins[nBins+1] = {10,15,20,25,30,60};
   
   const int nBinsy = 4;
-  double yBins[nBinsy+1] = {-1.465,-0.5,0.0,0.5,1.465};
+  double yBins[nBinsy+1] = {-2.4,-1.0,0.,1.0,2.4};
+
+
+void FakeInput(){
   
   TH1D *hPtMC = new TH1D("hPtMC","",nBins,ptBins);
   TH1D *hPtData = new TH1D("hPtData","",nBins,ptBins);
@@ -22,7 +23,7 @@ void FakeInput(){
   hPtData->SetBinContent(2,0.055);
   hPtData->SetBinContent(3,0.022);
   hPtData->SetBinContent(4,0.01);
-  hPtData->SetBinContent(5,0.0017);  
+  hPtData->SetBinContent(5,0.0022);  
   
   hPtMC->SetBinError(1,hPtMC->GetBinContent(1)*0.1);
   hPtMC->SetBinError(2,hPtMC->GetBinContent(2)*0.1);
@@ -37,15 +38,15 @@ void FakeInput(){
   hPtData->SetBinError(5,hPtData->GetBinContent(5)*0.1);
   
   
-  hyMC->SetBinContent(1,0.15);
-  hyMC->SetBinContent(2,0.14);
-  hyMC->SetBinContent(3,0.16);
-  hyMC->SetBinContent(4,0.17);
+  hyMC->SetBinContent(1,0.4);
+  hyMC->SetBinContent(2,0.3);
+  hyMC->SetBinContent(3,0.2);
+  hyMC->SetBinContent(4,0.1);
 
-  hyData->SetBinContent(1,0.15);
-  hyData->SetBinContent(2,0.13);
+  hyData->SetBinContent(1,0.42);
+  hyData->SetBinContent(2,0.28);
   hyData->SetBinContent(3,0.18);
-  hyData->SetBinContent(4,0.15);
+  hyData->SetBinContent(4,0.08);
   
   hyMC->SetBinError(1,hyMC->GetBinContent(1)*0.1);
   hyMC->SetBinError(2,hyMC->GetBinContent(2)*0.1);
@@ -57,6 +58,23 @@ void FakeInput(){
   hyData->SetBinError(3,hyData->GetBinContent(3)*0.1);
   hyData->SetBinError(4,hyData->GetBinContent(4)*0.1);
 
+
+  hPtMC->Scale(1./hPtMC->Integral());
+  hPtData->Scale(1./hPtData->Integral());
+  hyMC->Scale(1./hyMC->Integral());
+  hyData->Scale(1./hyData->Integral());
+  
+  cout<<"QUI"<<hPtMC->Integral()<<endl;
+  cout<<"QUI"<<hPtData->Integral()<<endl;
+  cout<<"QUI"<<hyMC->Integral()<<endl;
+  cout<<"QUI"<<hyData->Integral()<<endl;
+  
+
+  TH1D*hReweightDataOverMC_Pt=(TH1D*)hPtData->Clone("hReweightDataOverMC_Pt");
+  TH1D*hReweightDataOverMC_y=(TH1D*)hyData->Clone("hReweightDataOverMC_y");
+  
+  hReweightDataOverMC_Pt->Divide(hPtMC);
+  hReweightDataOverMC_y->Divide(hyMC);
 
   TCanvas*c=new TCanvas("c","c",800,500);
   c->Divide(2,2);
@@ -77,6 +95,50 @@ void FakeInput(){
   hPtMC->Write();
   hyData->Write();
   hyMC->Write();
+  hReweightDataOverMC_Pt->Write();
+  hReweightDataOverMC_y->Write();
 
 
+}
+
+
+void BuildFunctions(){
+
+  TFile*fout=new TFile("InputFilesMCData.root","read");
+  TH1D*hReweightDataOverMC_Pt=(TH1D*)fout->Get("hReweightDataOverMC_Pt");
+  TH1D*hReweightDataOverMC_y=(TH1D*)fout->Get("hReweightDataOverMC_y");
+
+  
+  TF1 *fPt = new TF1("fPt","pol2",10.,70.);
+  TF1 *fy = new TF1("fy","pol2",-1.4,1.4);
+
+  TCanvas*canvas=new TCanvas("canvas","canvas",1100,500);
+  canvas->Divide(2,1);
+  
+  canvas->cd(1);
+  
+  hReweightDataOverMC_Pt->SetMinimum(0);
+  hReweightDataOverMC_Pt->SetMaximum(1.5);
+  hReweightDataOverMC_Pt->Draw("p");
+  hReweightDataOverMC_Pt->SetMarkerStyle(22);
+  hReweightDataOverMC_Pt->SetMarkerSize(0.5);
+  hReweightDataOverMC_Pt->Fit("fPt");  
+
+  
+  canvas->cd(2);
+  
+  hReweightDataOverMC_y->SetMinimum(0);
+  hReweightDataOverMC_y->SetMaximum(1.5);
+  hReweightDataOverMC_y->Draw("p");
+  hReweightDataOverMC_y->SetMarkerStyle(22);
+  hReweightDataOverMC_y->SetMarkerSize(0.5);
+  hReweightDataOverMC_y->Fit("fy");  
+  
+  
+  TFile*fout=new TFile("FunctionsReweighting.root","recreate");
+  fout->cd();
+  hReweightDataOverMC_Pt->Write();
+  hReweightDataOverMC_y->Write();
+  fPt->Write();
+  fy->Write();
 }
