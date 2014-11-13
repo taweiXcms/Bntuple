fa492a67d445532bc882fc0de3af3829609c6fec
Jing Wang <wangj@lxplus0138.cern.ch>
2014-11-13 13:46:05 +0100

    org

--- CrossSection/Analysis/run.sh
+++
@@ -1,8 +0,0 @@
-CONFIGFILE=loop.C
-DESTINATION=$1
-OUTFILE=$3
-INFILE=$2
-
-#root -l -b -q  $CONFIGFILE++\(\"${INFILE}\",\"${OUTFILE}\"\)
-root -l -b -q  $CONFIGFILE+\(\"${INFILE}\",\"${OUTFILE}\"\)
-mv ${OUTFILE} ${DESTINATION}/${OUTFILE}
