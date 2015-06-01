e458cf07f161217efab235582af991b26e61bcaf
Ta Wei <ta-wei.wang@cern.ch>
2015-05-31 23:12:30 -0400

    add abs eta binning correction

--- CrossSection/Analysis/NuclearModification_DrawOnSamePad.C
+++ CrossSection/Analysis/NuclearModification_DrawOnSamePad.C
@@ -32,7 +32,8 @@ using namespace std;
   //Double_t  Bs_FFsysterror=0.6/10.4;//old BR
   Double_t  Bs_FFsysterror=0.6/10.5;//new BR
 //  Double_t  Bs_tagandprobcorrection[Bs_nbins]={1.041};
-  Double_t  Bs_tagandprobcorrection[Bs_nbins]={1.069};//new correction
+//  Double_t  Bs_tagandprobcorrection[Bs_nbins]={1.069};//new correction
+  Double_t  Bs_tagandprobcorrection[Bs_nbins]={1.065};//new correction, abs eta binning
 
   TString   B0_particle="Bzero";
   const int B0_nbins=3;
@@ -45,7 +46,8 @@ using namespace std;
   Double_t  B0_commonErrorN = TMath::Sqrt(0.035*0.035+0.046*0.046);
   Double_t  B0_FFsysterror=0.7/40.2;
 //  Double_t  B0_tagandprobcorrection[B0_nbins]={1.052,1.032,1.016};
-  Double_t  B0_tagandprobcorrection[B0_nbins]={1.078,1.061,1.058};//new correction
+//  Double_t  B0_tagandprobcorrection[B0_nbins]={1.078,1.061,1.058};//new correction
+  Double_t  B0_tagandprobcorrection[B0_nbins]={1.073,1.060,1.054};//new correction, abs eta binning
 
   TString   Bp_particle="Bplus";
   const int Bp_nbins=5;
@@ -59,7 +61,8 @@ using namespace std;
   //Double_t  Bp_FFsysterror=0.7/40.1;//old BR
   Double_t  Bp_FFsysterror=0.7/40.2;//new BR
 //  Double_t  Bp_tagandprobcorrection[Bp_nbins]={1.049,1.030,1.019,1.012,1.006};
-  Double_t  Bp_tagandprobcorrection[Bp_nbins]={1.071,1.057,1.058,1.045,1.061};//new correciton
+//  Double_t  Bp_tagandprobcorrection[Bp_nbins]={1.071,1.057,1.058,1.045,1.061};//new correciton
+  Double_t  Bp_tagandprobcorrection[Bp_nbins]={1.069,1.050,1.050,1.045,1.065};//new correciton, abs eta binning
 
   Double_t Bs_padratio=0.326899;
   Double_t B0_padratio=0.307285;
